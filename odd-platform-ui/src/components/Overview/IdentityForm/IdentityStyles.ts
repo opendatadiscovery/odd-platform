ab4f0cbf7a3a72ce7dc695b25a3a9e1a9b74e2e0
Sofia Shnaidman <sshnaidman@provectus.com>
2021-07-09 15:23:07 +0300

    Fixed suggested owner click callback in identity form.

--- odd-platform-ui/src/components/Overview/IdentityForm/IdentityStyles.ts
+++ odd-platform-ui/src/components/Overview/IdentityForm/IdentityStyles.ts
@@ -1,9 +1,15 @@
 import { Theme, createStyles, WithStyles } from '@material-ui/core';
 
+const maxContentWidth = '320px';
+
 export const styles = (theme: Theme) =>
   createStyles({
     container: {
       marginTop: theme.spacing(4),
+      padding: theme.spacing(4.5, 3, 4, 3),
+      border: '1px solid',
+      borderColor: '#EBECF0',
+      borderRadius: '8px',
     },
     captionIcon: {
       width: '76px',
@@ -14,11 +20,31 @@ export const styles = (theme: Theme) =>
     },
     formContainer: {
       width: '100%',
-      maxWidth: '320px',
+      maxWidth: maxContentWidth,
       marginTop: theme.spacing(1.75),
     },
     submitBtn: {
       marginTop: theme.spacing(2),
+    },
+    suggestedOwnersContainer: {
+      width: '100%',
+      maxWidth: maxContentWidth,
+      marginTop: theme.spacing(1),
+    },
+    suggestedOwnerItem: {
+      cursor: 'pointer',
+      padding: theme.spacing(0.25),
+      marginTop: theme.spacing(0.75),
+      '&:hover': {
+        backgroundColor: '#F4F5F7',
+        borderRadius: '4px',
+        '& > *': {
+          color: '#091E42',
+        },
+      },
+      '&:active': {
+        backgroundColor: '#EBECF0',
+      },
     },
   });
 
