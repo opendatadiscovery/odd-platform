6e54677d8e05ca114075ec104fa682757ac34879
Nikita Dementev <ndementev@provectus.com>
2021-07-23 14:33:52 +0300

    Fix DataQaulityTestRunRelation

--- odd-platform-api/src/main/java/com/provectus/oddplatform/repository/DataQualityTestRelationRepository.java
+++ odd-platform-api/src/main/java/com/provectus/oddplatform/repository/DataQualityTestRelationRepository.java
@@ -1,10 +1,8 @@
 package com.provectus.oddplatform.repository;
 
 import com.provectus.oddplatform.model.tables.pojos.DataQualityTestRelationsPojo;
-import com.provectus.oddplatform.model.tables.pojos.LineagePojo;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface DataQualityTestRelationRepository {
     void createRelations(final Collection<DataQualityTestRelationsPojo> pojos);
