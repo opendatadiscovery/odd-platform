ef75b00d06ff5ce417c9e330857739e53b3c4785
Roman Zabaluev <rzabaluev@provectus.com>
2021-09-30 15:09:07 +0300

    Add jooq-required constructor for TagDto

--- odd-platform-api/src/main/java/com/provectus/oddplatform/dto/TagDto.java
+++ odd-platform-api/src/main/java/com/provectus/oddplatform/dto/TagDto.java
@@ -1,6 +1,7 @@
 package com.provectus.oddplatform.dto;
 
 import com.provectus.oddplatform.model.tables.pojos.TagPojo;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
@@ -9,4 +10,18 @@ import lombok.Value;
 public class TagDto {
     TagPojo tagPojo;
     Long usedCount;
+
+    @SuppressWarnings("unused") // used by jooq
+    public TagDto(
+        final Long id,
+        final String name,
+        final Boolean important,
+        final Boolean isDeleted,
+        final LocalDateTime createdAt,
+        final LocalDateTime updatedAt,
+        final Long usedCount
+    ) {
+        this.tagPojo = new TagPojo(id, name, important, isDeleted, createdAt, updatedAt);
+        this.usedCount = usedCount;
+    }
 }
