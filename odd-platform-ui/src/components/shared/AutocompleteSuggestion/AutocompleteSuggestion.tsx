800ef1835a9c6d75d9356419b74c475f84990589
alexeykozyurov <akozyurov@provectus.com>
2021-11-01 12:32:32 +0300

    Refactored AutocompleteSuggestion to styled.

--- odd-platform-ui/src/components/shared/AutocompleteSuggestion/AutocompleteSuggestion.tsx
+++ odd-platform-ui/src/components/shared/AutocompleteSuggestion/AutocompleteSuggestion.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import * as S from 'components/shared/AutocompleteSuggestion/AutocompleteSuggestionStyles';
+import { SxProps } from '@mui/system';
+import { Theme } from '@mui/material';
 
 interface AutocompleteSuggestionProps {
   optionLabel: string;
   optionName: string | undefined;
+  sx?: SxProps<Theme>;
 }
 
 const AutocompleteSuggestion: React.FC<AutocompleteSuggestionProps> = ({
   optionLabel,
   optionName,
+  sx,
 }) => (
-  <S.Container variant="body2">
+  <S.Container sx={sx} variant="body2">
     <S.NoResultText>No result.</S.NoResultText>{' '}
     <S.CreateNewOptionText>
       Create new {optionLabel} &quot;{optionName}&quot;
