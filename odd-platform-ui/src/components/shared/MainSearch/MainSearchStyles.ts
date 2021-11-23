d307ac0b423490f39503770ada2e438e307f9dea
alexeykozyurov <akozyurov@provectus.com>
2021-11-23 17:59:28 +0300

    Refactored MainSearch to styled.

--- odd-platform-ui/src/components/shared/MainSearch/MainSearchStyles.ts
+++ odd-platform-ui/src/components/shared/MainSearch/MainSearchStyles.ts
@@ -1,51 +1,30 @@
-import { Theme } from '@mui/material';
+import { Autocomplete, autocompleteClasses } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
-import { WithStyles } from '@mui/styles';
-import createStyles from '@mui/styles/createStyles';
+export const Container = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  display: 'flex',
+  width: '100%',
+  maxWidth: '640px',
+}));
 
-export const searchHeight = 40;
-export const styles = (theme: Theme) =>
-  createStyles({
-    inputInput: {
-      height: `${searchHeight}px`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      border: '1px solid',
-      borderColor: theme.palette.button.primary.normal.border,
-      marginLeft: '4px',
-      '&:hover': {
-        border: '1px solid',
-        borderColor: theme.palette.button.primaryLight.normal.color,
-      },
-    },
-    clearIconContainer: {
-      right: '5px',
-      top: 'auto',
-    },
-    autocomplete: {
-      color: theme.palette.common.black,
-      width: '100%',
-      boxSizing: 'border-box',
-      '& .Mui-focused': {
-        border: '2px solid',
-        borderColor: theme.palette.button.primary.normal.border,
-      },
-    },
-    searchContainer: {
-      flexGrow: 1,
-      display: 'flex',
-      width: '100%',
-      maxWidth: '640px',
-    },
-    search: {
-      width: '100%',
-    },
-    suggestionItem: {
-      display: 'flex',
-      alignItems: 'center',
-      flexGrow: 1,
-    },
-    name: { marginRight: theme.spacing(1) },
-  });
+export const Search = styled('div')(({ theme }) => ({
+  width: '100%',
+}));
 
-export type StylesType = WithStyles<typeof styles>;
+export const SuggestionItem = styled(Link)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  flexGrow: 1,
+}));
+
+export const SearchAutocomplete = styled(Autocomplete)(({ theme }) => ({
+  color: theme.palette.common.black,
+  width: '100%',
+  boxSizing: 'border-box',
+  [`& .${autocompleteClasses.endAdornment}`]: {
+    right: '5px',
+    top: 'auto',
+  },
+}));
