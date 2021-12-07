7aaa524412218a4c5dc209e23c060663f2c71126
Nikita Dementev <ndementev@provectus.com>
2021-12-07 22:09:24 +0300

    Update migration script

--- odd-platform-api/src/main/resources/db/migration/V0_0_18__pull_push_data_sources.sql
+++ odd-platform-api/src/main/resources/db/migration/V0_0_18__pull_push_data_sources.sql
@@ -2,4 +2,8 @@ ALTER TABLE data_source
     DROP CONSTRAINT data_source_name_key,
     DROP CONSTRAINT data_source_oddrn_key,
     ALTER COLUMN active SET NOT NULL,
-    ALTER COLUMN active SET DEFAULT false
+    ALTER COLUMN active SET DEFAULT false;
+
+UPDATE data_source
+SET connection_url = NULL
+WHERE active IS false;
