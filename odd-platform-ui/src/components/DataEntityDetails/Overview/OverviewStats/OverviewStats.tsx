39ace4c44edba011d4c6ea8f0ec3b8c5b64f399f
alexeykozyurov <akozyurov@provectus.com>
2021-12-10 13:15:12 +0300

    Added support for OverviewDataInputStats.

--- odd-platform-ui/src/components/DataEntityDetails/Overview/OverviewStats/OverviewStats.tsx
+++ odd-platform-ui/src/components/DataEntityDetails/Overview/OverviewStats/OverviewStats.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import withStyles from '@mui/styles/withStyles';
 import { DataEntityTypeNameEnum } from 'generated-sources';
 import { DataEntityDetailsState } from 'redux/interfaces/dataentities';
 import OverviewDataConsumerStats from 'components/DataEntityDetails/Overview/OverviewStats/OverviewDataConsumerStats/OverviewDataConsumerStats';
-import { styles, StylesType } from './OverviewStatsStyles';
+import OverviewDataInputStats from 'components/DataEntityDetails/Overview/OverviewStats/OverviewDataInputStats/OverviewDataInputStats';
 import OverviewDatasetStats from './OverviewDatasetStats/OverviewDatasetStats';
 import OverviewTransformerStats from './OverviewTransformerStats/OverviewTransformerStats';
 import OverviewQualityTestStats from './OverviewQualityTestStats/OverviewQualityTestStats';
 
-interface OverviewStatsProps extends StylesType {
+interface OverviewStatsProps {
   dataEntityDetails: DataEntityDetailsState;
 }
 
 const OverviewStats: React.FC<OverviewStatsProps> = ({
-  classes,
   dataEntityDetails,
 }) => (
   <>
@@ -54,6 +52,14 @@ const OverviewStats: React.FC<OverviewStatsProps> = ({
               qualityTest={dataEntityDetails}
             />
           );
+        case DataEntityTypeNameEnum.INPUT:
+          return (
+            <OverviewDataInputStats
+              key={type.id}
+              outputs={dataEntityDetails.outputList}
+              unknownOutputsCount={dataEntityDetails.unknownOutputsCount}
+            />
+          );
         default:
           return null;
       }
@@ -61,4 +67,4 @@ const OverviewStats: React.FC<OverviewStatsProps> = ({
   </>
 );
 
-export default withStyles(styles)(OverviewStats);
+export default OverviewStats;
