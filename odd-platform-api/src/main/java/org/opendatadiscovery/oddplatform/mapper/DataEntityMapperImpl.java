53b3ebc5a65f452abafe073f699da9393762e52c
Nikita Dementev <ndementev@provectus.com>
2021-12-21 14:32:35 +0300

    Remove dimensions mapping logic for a time being

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/mapper/DataEntityMapperImpl.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/mapper/DataEntityMapperImpl.java
@@ -77,64 +77,11 @@ public class DataEntityMapperImpl implements DataEntityMapper {
             entity.setStats(mapStats(dto.getDataSetDetailsDto()));
         }
 
-        if (types.contains(DataEntityTypeDto.DATA_TRANSFORMER)) {
-            entity.setSourceList(dto.getDataTransformerDetailsDto().sourceList()
-                .stream()
-                .distinct()
-                .map(this::mapReference)
-                .collect(Collectors.toList()));
-
-            entity.setTargetList(dto.getDataTransformerDetailsDto()
-                .targetList()
-                .stream()
-                .distinct()
-                .map(this::mapReference)
-                .collect(Collectors.toList()));
-        }
-
-        if (types.contains(DataEntityTypeDto.DATA_QUALITY_TEST)) {
-            final DataQualityTestExpectation expectation = new DataQualityTestExpectation()
-                .type(dto.getDataQualityTestDetailsDto().expectationType());
-
-            expectation.putAll(MapUtils.emptyIfNull(dto.getDataQualityTestDetailsDto().expectationParameters()));
-
-            entity.expectation(expectation)
-                .datasetsList(dto.getDataQualityTestDetailsDto()
-                    .datasetList()
-                    .stream()
-                    .distinct()
-                    .map(this::mapReference)
-                    .collect(Collectors.toList()))
-                .linkedUrlList(dto.getDataQualityTestDetailsDto().linkedUrlList())
-                .latestRun(dataQualityMapper.mapDataQualityTestRun(
-                    dto.getDataEntity().getId(),
-                    dto.getDataQualityTestDetailsDto().latestTaskRun())
-                )
-                .suiteName(dto.getDataQualityTestDetailsDto().suiteName())
-                .suiteUrl(dto.getDataQualityTestDetailsDto().suiteUrl());
-        }
-
-        if (types.contains(DataEntityTypeDto.DATA_CONSUMER)) {
-            entity.setInputList(dto.getDataConsumerDetailsDto()
-                .inputList()
-                .stream()
-                .distinct()
-                .map(this::mapReference).collect(Collectors.toList()));
-        }
-
         if (types.contains(DataEntityTypeDto.DATA_ENTITY_GROUP) && dto.getGroupsDto() != null) {
             final List<DataEntityRef> dataEntityRefs = dto.getGroupsDto().entities().stream()
                 .map(this::mapReference)
                 .toList();
             entity.setEntities(dataEntityRefs);
-        }
-
-        if (types.contains(DataEntityTypeDto.DATA_INPUT)) {
-            entity.setOutputList(dto.getDataInputDetailsDto()
-                .outputList()
-                .stream()
-                .distinct()
-                .map(this::mapReference).collect(Collectors.toList()));
         }
 
         return entity;
