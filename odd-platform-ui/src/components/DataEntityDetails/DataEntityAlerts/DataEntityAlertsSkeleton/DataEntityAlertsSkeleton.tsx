3c51c116c8e0dbc98b0f29c9e3aa8e01dca97a04
alexeykozyurov <akozyurov@provectus.com>
2022-01-19 18:47:28 +0300

    DataEntityDetails alerts section refactored to styled.

--- odd-platform-ui/src/components/DataEntityDetails/DataEntityAlerts/DataEntityAlertsSkeleton/DataEntityAlertsSkeleton.tsx
+++ odd-platform-ui/src/components/DataEntityDetails/DataEntityAlerts/DataEntityAlertsSkeleton/DataEntityAlertsSkeleton.tsx
@@ -1,48 +1,43 @@
 import React from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import { Grid } from '@mui/material';
-import withStyles from '@mui/styles/withStyles';
-import cx from 'classnames';
-import { styles, StylesType } from './DataEntityAlertsSkeletonStyles';
+import { ColContainer } from '../DataEntityAlertsStyles';
 
-interface SkeletonProps extends StylesType {
+interface SkeletonProps {
   length?: number;
 }
 
-const AlertListSkeleton: React.FC<SkeletonProps> = ({
-  classes,
-  length,
-}) => {
+const AlertListSkeleton: React.FC<SkeletonProps> = ({ length }) => {
   const randomSkeletonWidth = () => {
     const rand = 75 + Math.random() * 15;
     return Math.round(rand);
   };
 
   const skeleton = (key: number) => (
-    <Grid key={key} container className={classes.container} wrap="nowrap">
-      <Grid item className={cx(classes.col, classes.colDate)}>
+    <Grid key={key} container sx={{ py: 1.25, px: 1 }} wrap="nowrap">
+      <ColContainer item $colType="date">
         <Skeleton width={`${randomSkeletonWidth()}%`} height="100%" />
-      </Grid>
-      <Grid item className={cx(classes.col, classes.colType)}>
+      </ColContainer>
+      <ColContainer item $colType="type">
         <Skeleton width={`${randomSkeletonWidth()}%`} height="100%" />
-      </Grid>
-      <Grid item className={cx(classes.col, classes.colDescription)}>
+      </ColContainer>
+      <ColContainer item $colType="description">
         <Skeleton width={`${randomSkeletonWidth()}%`} height="100%" />
-      </Grid>
-      <Grid item className={cx(classes.col, classes.colStatus)}>
+      </ColContainer>
+      <ColContainer item $colType="status">
         <Skeleton width={`${randomSkeletonWidth()}%`} height="100%" />
-      </Grid>
-      <Grid item className={cx(classes.col, classes.colUpdatedBy)}>
+      </ColContainer>
+      <ColContainer item $colType="updatedBy">
         <Skeleton width={`${randomSkeletonWidth()}%`} height="100%" />
-      </Grid>
-      <Grid item className={cx(classes.col, classes.colUpdatedTime)}>
+      </ColContainer>
+      <ColContainer item $colType="updatedTime">
         <Skeleton width={`${randomSkeletonWidth()}%`} height="100%" />
-      </Grid>
-      <Grid item className={cx(classes.col, classes.colActionBtn)} />
+      </ColContainer>
+      <ColContainer item $colType="actionBtn" />
     </Grid>
   );
 
   return <>{[...Array(length)].map((_, id) => skeleton(id))}</>;
 };
 
-export default withStyles(styles)(AlertListSkeleton);
+export default AlertListSkeleton;
