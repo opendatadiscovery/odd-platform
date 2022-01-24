461f87f28d5e8815f8d7877db9b0888a61fdbb4d
Chismur <32108047+Chismur@users.noreply.github.com>
2022-01-24 21:07:18 +0300

    [Backend] - Integration tests. Initial commit (#309)
    * [Backend] - Integration tests. Initial commit
    
    * [Backend] - Integration tests. Review issues fix:
    1. @Sql added instead of method deleteAll()
    2. Removed redundant annotations
    3. Removed redundant dependencies
    
    * [Backend] - Integration tests. Checkstyle fix
    
    * [Backend] - Integration tests. Checkstyle fix
    
    * Fix duplicate fetching and ingestion of specific attributes (#313)
    
    * Fixed bug with datasource form validation. (#311)
    
    * Fix nested lists in specific_attributes (#314)
    
    * Feature: DEG integration (#274)
    
    * Uplift Ingestion Contract (#316)
    
    * Add services subtypes (#317)
    
    * Add services subtypes
    
    * Add services subtypes into internal OpenAPI specification
    
    * remove logic of lowercasing oddrns (#318)
    
    * Fix missing 'FROM' clause (#319)
    
    * Bugfix: Lineage cycle dependency (#320)
    
    * Implemented cycle dependency filtering of lineage edges.
    
    * Deleted commented lines.
    
    * Fixed some styles.
    
    * Remove types from database (#321)
    
    * Update build.yaml
    
    * Search page refactored to styled. (#283)
    
    * Search filters refactored to styled. (#287)
    
    * Dataset Field update doesn't update TSVectors (#325)
    
    * LDAP authentication (#293)
    
    * Fix NPE in Ingestion API (#326)
    
    * Fix alerts totals (#328)
    
    * tests base
    
    * checkstyle fix
    
    * [Backend] - Integration tests. Minor changes
    
    * [Backend] - Integration tests. Test on update, bulk create/update were added. Minor change in create/update implementation
    
    * [Backend] - Integration tests. Revew issues fix
    
    Co-authored-by: DementevNikita <53081563+DementevNikita@users.noreply.github.com>
    Co-authored-by: Alexey Kozyurov <akozyurov@provectus.com>
    Co-authored-by: Nikita Dementev <ndementev@provectus.com>
    Co-authored-by: Roman Zabaluev <rzabaluev@provectus.com>
    Co-authored-by: damirabdullin <dabdullin@provectus.com>

---
+++ odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/repository/OwnerRepositoryImplTest.java
@@ -0,0 +1,14 @@
+package org.opendatadiscovery.oddplatform.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.opendatadiscovery.oddplatform.BaseIntegrationTest;
+
+@DisplayName("Integration tests for OwnerRepository")
+class OwnerRepositoryImplTest extends BaseIntegrationTest {
+
+    @Test
+    void name() {
+        //test
+    }
+}
