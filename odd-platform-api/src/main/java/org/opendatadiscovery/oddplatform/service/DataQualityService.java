154b65a8e3a7da39a4663554529bc1f13078d15d
damirabdullin <dabdullin@provectus.com>
2022-01-26 21:53:32 +0300

    data quality test run pagination backend

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/DataQualityService.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/DataQualityService.java
@@ -2,6 +2,7 @@ package org.opendatadiscovery.oddplatform.service;
 
 import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityList;
 import org.opendatadiscovery.oddplatform.api.contract.model.DataQualityTestRunList;
+import org.opendatadiscovery.oddplatform.api.contract.model.DataQualityTestRunStatus;
 import org.opendatadiscovery.oddplatform.api.contract.model.DataSetTestReport;
 import reactor.core.publisher.Mono;
 
@@ -10,5 +11,8 @@ public interface DataQualityService {
 
     Mono<DataSetTestReport> getDatasetTestReport(final long datasetId);
 
-    Mono<DataQualityTestRunList> getDataQualityTestRuns(final long dataQualityTestId);
+    Mono<DataQualityTestRunList> getDataQualityTestRuns(final long dataQualityTestId,
+                                                        final DataQualityTestRunStatus status,
+                                                        final Integer page,
+                                                        final Integer size);
 }
