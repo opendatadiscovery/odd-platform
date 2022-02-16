5092a78f4d2fdaf2c8595a5ae37730414787db99
Maxim Tereshin <tereshin93@gmail.com>
2022-02-16 15:08:48 +0300

    Remove token actions

--- odd-platform-ui/src/redux/actions/tokens.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncAction } from 'typesafe-actions';
-import { DataSource, Token } from 'generated-sources';
-
-export const updateTokenAction = createAsyncAction(
-  'PUT_TOKEN__REQUEST',
-  'PUT_TOKEN__SUCCESS',
-  'PUT_TOKEN__FAILURE'
-)<undefined, DataSource, undefined>();
