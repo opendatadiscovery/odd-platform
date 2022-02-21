52b81447d2e68a315577bab004f1ec225d90c752
damirabdullin <dabdullin@provectus.com>
2022-02-21 14:15:30 +0300

    generate tokens for existing datasources

--- odd-platform-api/src/main/resources/db/migration/V0_0_28__add_token.sql
+++ odd-platform-api/src/main/resources/db/migration/V0_0_28__add_token.sql
@@ -11,3 +11,14 @@ CREATE TABLE IF NOT EXISTS token
 ALTER TABLE data_source
     ADD COLUMN IF NOT EXISTS token_id bigint,
     ADD CONSTRAINT data_source_fk_token FOREIGN KEY (token_id) REFERENCES token (id);
+
+WITH insertedTokenCTE AS (INSERT INTO token (value) SELECT 'change' from data_source RETURNING id),
+     tokenRowCTE as (SELECT id as tokenID, row_number() over () as row_number from insertedTokenCTE),
+     dataSourceCTE as (select id, row_number() over () as row_number, token_id from data_source)
+UPDATE data_source ds
+SET token_id = (
+    select tokenRowCTE.tokenID
+    from tokenRowCTE
+             join dataSourceCTE on tokenRowCTE.row_number = dataSourceCTE.row_number
+    where dataSourceCTE.id = ds.id
+);
