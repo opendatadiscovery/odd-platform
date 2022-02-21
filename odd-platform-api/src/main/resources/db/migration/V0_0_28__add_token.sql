6443885d6bda429171307937ac5c8ef83378ac3f
damirabdullin <dabdullin@provectus.com>
2022-02-21 14:22:52 +0300

    sql script fix

--- odd-platform-api/src/main/resources/db/migration/V0_0_28__add_token.sql
+++ odd-platform-api/src/main/resources/db/migration/V0_0_28__add_token.sql
@@ -12,13 +12,13 @@ ALTER TABLE data_source
     ADD COLUMN IF NOT EXISTS token_id bigint,
     ADD CONSTRAINT data_source_fk_token FOREIGN KEY (token_id) REFERENCES token (id);
 
-WITH insertedTokenCTE AS (INSERT INTO token (value) SELECT 'change' from data_source RETURNING id),
-     tokenRowCTE as (SELECT id as tokenID, row_number() over () as row_number from insertedTokenCTE),
-     dataSourceCTE as (select id, row_number() over () as row_number, token_id from data_source)
+WITH insertedTokenCTE AS (INSERT INTO token (value) SELECT 'change' FROM data_source RETURNING id),
+     tokenRowCTE AS (SELECT id AS tokenID, row_number() OVER () AS row_number FROM insertedTokenCTE),
+     dataSourceCTE AS (SELECT id, row_number() OVER () AS row_number, token_id FROM data_source)
 UPDATE data_source ds
 SET token_id = (
-    select tokenRowCTE.tokenID
-    from tokenRowCTE
-             join dataSourceCTE on tokenRowCTE.row_number = dataSourceCTE.row_number
-    where dataSourceCTE.id = ds.id
+    SELECT tokenRowCTE.tokenID
+    FROM tokenRowCTE
+             JOIN dataSourceCTE ON tokenRowCTE.row_number = dataSourceCTE.row_number
+    WHERE dataSourceCTE.id = ds.id
 );
