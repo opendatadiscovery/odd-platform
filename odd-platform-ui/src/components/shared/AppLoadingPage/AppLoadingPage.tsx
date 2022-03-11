ade5fc03c14d2de913a6267fef13a340c20b6518
alinamiryuk <alinamiryuk@mail.ru>
2022-03-11 15:54:37 +0300

    AppLoadingPage test

--- odd-platform-ui/src/components/shared/AppLoadingPage/AppLoadingPage.tsx
+++ odd-platform-ui/src/components/shared/AppLoadingPage/AppLoadingPage.tsx
@@ -3,7 +3,7 @@ import AppCircularProgress from 'components/shared/AppCircularProgress/AppCircul
 import { Container } from './AppLoadingPageStyles';
 
 const AppLoadingPage: React.FC = () => (
-  <Container container>
+  <Container container aria-label="AppLoadingPage">
     <AppCircularProgress
       size={70}
       background="transparent"
