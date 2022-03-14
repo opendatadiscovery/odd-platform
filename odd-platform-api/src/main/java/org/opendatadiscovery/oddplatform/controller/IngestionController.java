a1080397d6d25cde8d2c56191435226842b69e62
Nikita Dementev <ndementev@provectus.com>
2022-03-14 15:26:56 +0300

    Align data source collector request with the latest ODD Spec

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/controller/IngestionController.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/controller/IngestionController.java
@@ -5,13 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.opendatadiscovery.oddplatform.ingestion.contract.api.IngestionApi;
 import org.opendatadiscovery.oddplatform.ingestion.contract.model.DataEntityList;
-import org.opendatadiscovery.oddplatform.ingestion.contract.model.DataSource;
+import org.opendatadiscovery.oddplatform.ingestion.contract.model.DataSourceList;
 import org.opendatadiscovery.oddplatform.service.DataSourceIngestionService;
 import org.opendatadiscovery.oddplatform.service.IngestionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -35,12 +34,11 @@ public class IngestionController implements IngestionApi {
     }
 
     @Override
-    public Mono<ResponseEntity<Void>> createDataSource(@Valid final Flux<DataSource> dataSource,
+    public Mono<ResponseEntity<Void>> createDataSource(@Valid final Mono<DataSourceList> dataSourceList,
                                                        final ServerWebExchange exchange) {
-        return dataSource
+        return dataSourceList
             .publishOn(Schedulers.boundedElastic())
-            .collectList()
-            .flatMap(dataSourceIngestionService::createDataSourcesFromIngestion)
+            .flatMap(model -> dataSourceIngestionService.createDataSourcesFromIngestion(model.getItems()))
             .map(ignored -> ResponseEntity.ok().build());
     }
 }
