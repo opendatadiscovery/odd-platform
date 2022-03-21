927d83af8a592f3d821d78d520095a7202e54af5
alinamiryuk <alinamiryuk@mail.ru>
2022-03-21 17:34:54 +0300

    Collectors Skeleton

---
+++ odd-platform-ui/src/components/Management/CollectorsList/CollectorSkeletonItem/CollectorSkeletonItem.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import { Grid } from '@mui/material';
+import { mainSkeletonHeight } from 'lib/constants';
+import {
+  Container,
+  DescriptionContainer,
+} from '../CollectorItem/CollectorItemStyles';
+
+interface SkeletonProps {
+  width: string;
+}
+
+const CollectorSkeletonItem: React.FC<SkeletonProps> = ({ width }) => (
+  <Container sx={{ mb: 1 }}>
+    <Grid container alignItems="flex-start" spacing={2}>
+      <Grid item container xs={12}>
+        <Grid item xs={2}>
+          <Skeleton width={width} height={mainSkeletonHeight} />
+        </Grid>
+      </Grid>
+      <DescriptionContainer item xs={6} container>
+        <Skeleton width={width} height={mainSkeletonHeight} />
+        <Skeleton width={width} height={mainSkeletonHeight} />
+        <Skeleton width={width} height={mainSkeletonHeight} />
+      </DescriptionContainer>
+      <DescriptionContainer item xs={6} container>
+        <Skeleton width={width} height={mainSkeletonHeight} />
+        <Skeleton width={width} height={mainSkeletonHeight} />
+        <Skeleton width={width} height={mainSkeletonHeight} />
+      </DescriptionContainer>
+    </Grid>
+  </Container>
+);
+export default CollectorSkeletonItem;
