3d13a1a494410de602aed174528893b468baf8e2
alinamiryuk <alinamiryuk@mail.ru>
2022-04-11 16:53:13 +0300

    terms component

---
+++ odd-platform-ui/src/components/Terms/TermsStyles.ts
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material';
+import { maxSidebarWidth } from 'lib/constants';
+import styled from 'styled-components';
+
+export const TermsContainer = styled('div')(({ theme }) => ({
+  paddingTop: theme.spacing(2),
+}));
+
+export const TermsContentContainer = styled(Grid)(({ theme }) => ({
+  paddingLeft: `${maxSidebarWidth}px`,
+  paddingRight: theme.spacing(2),
+}));
+
+export const TermsFiltersContainer = styled(Grid)(({ theme }) => ({
+  width: '100%',
+  maxWidth: `${maxSidebarWidth}px`,
+  position: 'fixed',
+  left: 0,
+  paddingRight: theme.spacing(2),
+}));
+
+export const TermsResultsContainer = styled(Grid)(({ theme }) => ({
+  flexGrow: 1,
+  maxWidth: '100%',
+  marginLeft: theme.spacing(1),
+}));
