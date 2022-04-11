a75c3388e683bacff1aa8b485f4b888f9bef4958
Nikita Dementev <ndementev@provectus.com>
2022-04-11 16:15:09 +0300

    Namespace service tests

--- odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/service/NamespaceServiceImplTest.java
+++ odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/service/NamespaceServiceImplTest.java
@@ -5,18 +5,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendatadiscovery.oddplatform.api.contract.model.Namespace;
 import org.opendatadiscovery.oddplatform.api.contract.model.NamespaceFormData;
+import org.opendatadiscovery.oddplatform.api.contract.model.NamespaceUpdateFormData;
+import org.opendatadiscovery.oddplatform.exception.NotFoundException;
 import org.opendatadiscovery.oddplatform.mapper.NamespaceMapper;
-import org.opendatadiscovery.oddplatform.mapper.NamespaceMapperImpl;
 import org.opendatadiscovery.oddplatform.model.tables.pojos.NamespacePojo;
 import org.opendatadiscovery.oddplatform.repository.reactive.ReactiveDataSourceRepository;
 import org.opendatadiscovery.oddplatform.repository.reactive.ReactiveNamespaceRepository;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -25,6 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -91,6 +91,129 @@ class NamespaceServiceImplTest {
     @Test
     @DisplayName("Creates a namespace by calling the namespace repository")
     public void testCreate() {
+        final long namespaceId = 1L;
+        final NamespaceFormData form = new NamespaceFormData()
+            .name(UUID.randomUUID().toString());
 
+        final NamespacePojo mappedForm = new NamespacePojo()
+            .setName(form.getName());
+
+        final NamespacePojo createdPojo = new NamespacePojo()
+            .setId(namespaceId)
+            .setName(form.getName())
+            .setIsDeleted(false)
+            .setCreatedAt(LocalDateTime.now());
+
+        final Namespace expected = new Namespace()
+            .id(namespaceId)
+            .name(form.getName());
+
+        when(namespaceRepository.create(eq(mappedForm))).thenReturn(Mono.just(createdPojo));
+        when(namespaceMapper.mapForm(eq(form))).thenReturn(mappedForm);
+        when(namespaceMapper.mapPojo(eq(createdPojo))).thenReturn(expected);
+
+        StepVerifier.create(namespaceService.create(form))
+            .assertNext(actual -> assertThat(actual).isEqualTo(expected))
+            .verifyComplete();
+
+        verify(namespaceRepository, only()).create(eq(mappedForm));
+        verify(namespaceMapper, times(1)).mapForm(eq(form));
+        verify(namespaceMapper, times(1)).mapPojo(eq(createdPojo));
+    }
+
+    @Test
+    @DisplayName("Updates an exising namespace in the database")
+    public void testUpdateExistingNamespace() {
+        final long namespaceId = 1L;
+
+        final String newName = UUID.randomUUID().toString();
+
+        final NamespaceUpdateFormData form = new NamespaceUpdateFormData().name(newName);
+
+        final NamespacePojo namespace = new NamespacePojo()
+            .setId(namespaceId)
+            .setName(UUID.randomUUID().toString())
+            .setCreatedAt(LocalDateTime.now())
+            .setIsDeleted(false)
+            .setUpdatedAt(LocalDateTime.now());
+
+        final NamespacePojo appliedFormPojo = new NamespacePojo()
+            .setName(newName);
+
+        final NamespacePojo updatedPojo = new NamespacePojo(namespace)
+            .setName(newName)
+            .setUpdatedAt(LocalDateTime.now());
+
+        final Namespace expected = new Namespace()
+            .id(namespaceId)
+            .name(newName);
+
+        when(namespaceRepository.get(eq(namespaceId))).thenReturn(Mono.just(namespace));
+        when(namespaceMapper.applyForm(eq(namespace), eq(form))).thenReturn(appliedFormPojo);
+        when(namespaceRepository.update(eq(appliedFormPojo))).thenReturn(Mono.just(updatedPojo));
+        when(namespaceMapper.mapPojo(eq(updatedPojo))).thenReturn(expected);
+
+        StepVerifier.create(namespaceService.update(namespaceId, form))
+            .assertNext(actual -> assertThat(actual).isEqualTo(expected))
+            .verifyComplete();
+
+        verify(namespaceRepository, times(1)).get(eq(namespaceId));
+        verify(namespaceRepository, times(1)).update(eq(appliedFormPojo));
+        verify(namespaceMapper, times(1)).applyForm(eq(namespace), eq(form));
+        verify(namespaceMapper, times(1)).mapPojo(eq(updatedPojo));
+    }
+
+    @Test
+    @DisplayName("Tries to update nonexistent namespace in the database and fails with an error")
+    public void testUpdateNonExistingNamespace() {
+        final long nonExistentNamespaceId = 1L;
+
+        when(namespaceRepository.get(eq(nonExistentNamespaceId))).thenReturn(Mono.empty());
+
+        namespaceService.update(nonExistentNamespaceId, new NamespaceUpdateFormData())
+            .as(StepVerifier::create)
+            .verifyError(NotFoundException.class);
+
+        verify(namespaceRepository, times(1)).get(eq(nonExistentNamespaceId));
+        verify(namespaceRepository, never()).update(any());
+        verify(namespaceMapper, never()).applyForm(any(), any());
+        verify(namespaceMapper, never()).mapPojo(any());
+    }
+
+    @Test
+    @DisplayName("Deletes a namespace which isn't tied with any data sources from the database")
+    public void testDelete() {
+        final long namespaceId = 1L;
+
+        final NamespacePojo namespace = new NamespacePojo()
+            .setId(namespaceId)
+            .setDeletedAt(LocalDateTime.now())
+            .setIsDeleted(true);
+
+        when(dataSourceRepository.existsByNamespace(eq(namespaceId))).thenReturn(Mono.just(false));
+        when(namespaceRepository.delete(eq(namespaceId))).thenReturn(Mono.just(namespace));
+
+        namespaceService.delete(namespaceId)
+            .as(StepVerifier::create)
+            .assertNext(deletedNamespaceId -> assertThat(deletedNamespaceId).isEqualTo(namespaceId))
+            .verifyComplete();
+
+        verify(namespaceRepository, only()).delete(eq(namespaceId));
+        verify(dataSourceRepository, only()).existsByNamespace(eq(namespaceId));
+    }
+
+    @Test
+    @DisplayName("Tries to delete a namespace which is tied with existing data sources and fails with an error")
+    public void testDeleteTiedNamespace() {
+        final long namespaceId = 1L;
+
+        when(dataSourceRepository.existsByNamespace(eq(namespaceId))).thenReturn(Mono.just(true));
+
+        namespaceService.delete(namespaceId)
+            .as(StepVerifier::create)
+            .verifyError(IllegalStateException.class);
+
+        verify(namespaceRepository, never()).delete(eq(namespaceId));
+        verify(dataSourceRepository, only()).existsByNamespace(eq(namespaceId));
     }
 }
