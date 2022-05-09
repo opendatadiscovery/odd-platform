d3538f1bdbc645045c8078d726e837cd4d30debc
Nikita Dementev <ndementev@provectus.com>
2022-05-09 22:58:37 +0300

    Add custom OAuth login page

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/config/OAuthSecurityConfiguration.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/config/OAuthSecurityConfiguration.java
@@ -1,19 +1,38 @@
 package org.opendatadiscovery.oddplatform.config;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.opendatadiscovery.oddplatform.auth.CognitoOidcLogoutSuccessHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.context.webflux.SpringWebFluxContext;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers;
 
 @Configuration
 @ConditionalOnProperty(value = "auth.type", havingValue = "OAUTH2")
@@ -33,47 +52,94 @@ public class OAuthSecurityConfiguration {
     }
 
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SecurityWebFilterChain securityWebFilterChainOauth2ResourceServer(final ServerHttpSecurity http,
-                                                                             final ReactiveClientRegistrationRepository
-                                                                                 repo) {
-        final ServerLogoutSuccessHandler logoutHandler = cognitoEnabled
-            ? new CognitoOidcLogoutSuccessHandler(logoutUrl, clientId)
-            : new OidcClientInitiatedServerLogoutSuccessHandler(repo);
-
-        return http
-            .cors().and()
-            .csrf().disable()
-            .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/ingestion/**"))
-            .authorizeExchange(e -> e
-                .pathMatchers("/ingestion/entities").permitAll()
-                .pathMatchers("/actuator/**").permitAll()
-                .pathMatchers("/**").authenticated())
-            .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
-            .logout()
-            .logoutSuccessHandler(logoutHandler)
-            .and()
-            .build();
-    }
-
-    @Bean
     public SecurityWebFilterChain securityWebFilterChainOauth2Client(final ServerHttpSecurity http,
-                                                                     final ReactiveClientRegistrationRepository repo) {
+                                                                     final ReactiveClientRegistrationRepository repo,
+                                                                     final TemplateEngine templateEngine) {
         final ServerLogoutSuccessHandler logoutHandler = cognitoEnabled
             ? new CognitoOidcLogoutSuccessHandler(logoutUrl, clientId)
             : new OidcClientInitiatedServerLogoutSuccessHandler(repo);
 
-        return http
+        final List<ClientRegistration> clientRegistrations =
+            IteratorUtils.toList(((InMemoryReactiveClientRegistrationRepository) repo).iterator());
+
+        ServerHttpSecurity sec = http
             .cors().and()
             .csrf().disable()
             .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/**"))
             .authorizeExchange(e -> e
-                .pathMatchers("/health", "/favicon.ico").permitAll()
+                .pathMatchers("/actuator/**", "/favicon.ico", "/ingestion/**", "/img/**").permitAll()
                 .pathMatchers("/**").authenticated())
             .oauth2Login(Customizer.withDefaults())
-            .logout()
-            .logoutSuccessHandler(logoutHandler)
-            .and()
-            .build();
+            .logout().logoutSuccessHandler(logoutHandler)
+            .and();
+
+        if (clientRegistrations.size() > 1) {
+            sec = sec
+                .addFilterAfter(new LoginPageFilter(templateEngine, clientRegistrations), SecurityWebFiltersOrder.CSRF);
+        }
+
+        return sec.build();
+    }
+
+    private static class LoginPageFilter implements WebFilter {
+        private static final String CLIENT_REGISTRATION_IMAGE_SOURCE = "/img/%s.png";
+        private static final String CLIENT_REGISTRATION_HREF = "/oauth2/authorization/%s";
+
+        private final ServerWebExchangeMatcher matcher = pathMatchers(HttpMethod.GET, "/login");
+
+        private final TemplateEngine templateEngine;
+        private final List<ClientRegistrationModel> clientRegistrations;
+
+        public LoginPageFilter(final TemplateEngine templateEngine,
+                               final List<ClientRegistration> clientRegistrations) {
+            this.clientRegistrations = clientRegistrations.stream()
+                .map(this::mapClientRegistration)
+                .toList();
+
+            this.templateEngine = templateEngine;
+        }
+
+        @Override
+        public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain) {
+            return this.matcher.matches(exchange)
+                .filter(ServerWebExchangeMatcher.MatchResult::isMatch)
+                .switchIfEmpty(chain.filter(exchange).then(Mono.empty()))
+                .flatMap(matchResult -> render(exchange));
+        }
+
+        protected Mono<Void> render(final ServerWebExchange exchange) {
+            final ServerHttpResponse result = exchange.getResponse();
+            result.setStatusCode(HttpStatus.OK);
+            result.getHeaders().setContentType(MediaType.TEXT_HTML);
+
+            return result.writeWith(response(exchange).map(r -> exchange.getResponse().bufferFactory().wrap(r)));
+        }
+
+        protected Mono<byte[]> response(final ServerWebExchange exchange) {
+            final SpringWebFluxContext context =
+                new SpringWebFluxContext(exchange, null, resolveContextVariables(exchange));
+
+            return Mono.just(
+                this.templateEngine.process("oauth2_login.html", context).getBytes(StandardCharsets.UTF_8)
+            );
+        }
+
+        private Map<String, Object> resolveContextVariables(final ServerWebExchange exchange) {
+            return Map.of(
+                "error", exchange.getRequest().getQueryParams().containsKey("error"),
+                "regs", clientRegistrations
+            );
+        }
+
+        private ClientRegistrationModel mapClientRegistration(final ClientRegistration cr) {
+            return new ClientRegistrationModel(
+                StringUtils.defaultIfBlank(cr.getClientName(), cr.getRegistrationId()),
+                CLIENT_REGISTRATION_IMAGE_SOURCE.formatted(cr.getRegistrationId()),
+                CLIENT_REGISTRATION_HREF.formatted(cr.getRegistrationId())
+            );
+        }
+
+        private record ClientRegistrationModel(String name, String imgSrc, String href) {
+        }
     }
 }
