dde22bdf2e3e99203813d880a05c2ba990dcb4b9
alexeykozyurov <akozyurov@provectus.com>
2022-05-19 18:39:50 +0300

    Refactored NumberFormatted component. Added number formatting to row column at search results page. Fixed bug with tag item layout. Disabled "load more" button for DEGs in lineage.

--- odd-platform-ui/src/components/Management/TagsList/EditableTagItem/EditableTagItemStyles.ts
+++ odd-platform-ui/src/components/Management/TagsList/EditableTagItem/EditableTagItemStyles.ts
@@ -2,11 +2,11 @@ import { Grid } from '@mui/material';
 import styled from 'styled-components';
 
 export const ActionsContainer = styled(Grid)(() => ({
+  justifyContent: 'flex-end',
   opacity: 0,
 }));
 
 export const Container = styled(Grid)(({ theme }) => ({
-  justifyContent: 'space-between',
   flexWrap: 'nowrap',
   alignItems: 'center',
   padding: theme.spacing(1.5, 1, 1.5, 1),
