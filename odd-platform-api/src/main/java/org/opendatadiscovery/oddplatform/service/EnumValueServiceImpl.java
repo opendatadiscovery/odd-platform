ed57033c665805ab43608170a7b0b4fccbae137b
Andrey Esmeneev <a.esmeneev@gmail.com>
2022-05-25 19:18:43 +0300

    Move enums updating logic from repository to service

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/EnumValueServiceImpl.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/EnumValueServiceImpl.java
@@ -1,12 +1,15 @@
 package org.opendatadiscovery.oddplatform.service;
 
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import org.opendatadiscovery.oddplatform.api.contract.model.EnumValue;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.BooleanUtils;
 import org.opendatadiscovery.oddplatform.api.contract.model.EnumValueFormData;
 import org.opendatadiscovery.oddplatform.api.contract.model.EnumValueList;
 import org.opendatadiscovery.oddplatform.mapper.EnumValueMapper;
-import org.opendatadiscovery.oddplatform.repository.EnumValueRepository;
+import org.opendatadiscovery.oddplatform.model.tables.pojos.EnumValuePojo;
+import org.opendatadiscovery.oddplatform.repository.reactive.ReactiveEnumValueRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,28 +17,57 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 public class EnumValueServiceImpl implements EnumValueService {
-    private final EnumValueRepository enumValueRepository;
+    private final ReactiveEnumValueRepository reactiveEnumValueRepository;
     private final EnumValueMapper mapper;
 
     @Override
     public Mono<EnumValueList> createEnumValues(final Long datasetFieldId, final List<EnumValueFormData> formData) {
-        return Flux.fromStream(formData.stream())
-            .map(fd -> mapper.mapForm(fd, datasetFieldId))
+        final var source =  Flux.fromStream(formData.stream())
+            .map(fd -> mapper.mapToPojo(fd, datasetFieldId)).cache();
+
+        final var hasDuplicates = source
+            .map(EnumValuePojo::getName)
             .collectList()
-            .map(list -> enumValueRepository.updateFieldEnumValues(datasetFieldId, list))
-            .map(pojos -> {
-                final List<EnumValue> items = pojos.stream().map(mapper::mapPojo).toList();
-                return new EnumValueList().items(items);
-            });
+            .map(this::hasDuplicates)
+            .filter(BooleanUtils::isFalse)
+            .switchIfEmpty(Mono.error(new RuntimeException("There are duplicates in enum values")));
+
+        final var deletedEnumValues = source
+            .mapNotNull(EnumValuePojo::getId)
+            .filter(Objects::nonNull)
+            .collectList()
+            .map(list -> reactiveEnumValueRepository.softDeleteOutdatedEnumValues(datasetFieldId, list));
+
+        final var updatedEnumValues = source
+            .filter(pojo -> pojo.getId() != null)
+            .collectList()
+            .map(reactiveEnumValueRepository::bulkUpdate);
+
+        final var createdEnumValues = source
+            .filter(pojo -> pojo.getId() == null)
+            .collectList()
+            .map(reactiveEnumValueRepository::bulkCreate);
+
+        return hasDuplicates
+            .then(
+                Flux.concat(deletedEnumValues, updatedEnumValues, createdEnumValues)
+                .flatMap(t -> t.filter(f -> !f.getIsDeleted()).map(mapper::mapToEnum))
+                .collectList()
+                .map(list -> new EnumValueList().items(list)));
     }
 
     @Override
     public Mono<EnumValueList> getEnumValues(final Long datasetFieldId) {
-        return Mono.just(datasetFieldId)
-            .map(fieldId -> enumValueRepository.getEnumValuesByFieldId(datasetFieldId))
-            .map(pojos -> {
-                final List<EnumValue> enumValues = pojos.stream().map(mapper::mapPojo).toList();
-                return new EnumValueList().items(enumValues);
-            });
+        return reactiveEnumValueRepository.getEnumValuesByFieldId(datasetFieldId)
+            .map(mapper::mapToEnum)
+            .collectList()
+            .map(enumValues -> new EnumValueList().items(enumValues));
+    }
+
+    private <T> boolean hasDuplicates(final List<T> sourceList) {
+        if (CollectionUtils.isEmpty(sourceList)) {
+            return false;
+        }
+        return sourceList.stream().distinct().count() != sourceList.size();
     }
 }
