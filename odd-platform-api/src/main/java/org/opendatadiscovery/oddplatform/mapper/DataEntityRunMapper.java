26930cb02da2b2e3b6460915b5e7f6739f13221a
Nikita Dementev <ndementev@provectus.com>
2022-06-10 14:50:19 +0300

    Replace .collect() with toList()

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/mapper/DataEntityRunMapper.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/mapper/DataEntityRunMapper.java
@@ -22,6 +22,6 @@ public interface DataEntityRunMapper {
                 .hasNext(page.isHasNext()))
             .items(page.getData().stream()
                 .map(r -> mapDataEntityRun(dataQualityTestId, r))
-                .collect(Collectors.toList()));
+                .toList());
     }
 }
