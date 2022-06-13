f3346abef41ae8dad40cc52b62042948f550d0ad
Nikita Dementev <ndementev@provectus.com>
2022-06-13 19:54:39 +0300

    Fix migration

--- odd-platform-api/src/main/resources/db/migration/V0_0_43__additional_keys_to_dq_test_relation.sql
+++ odd-platform-api/src/main/resources/db/migration/V0_0_43__additional_keys_to_dq_test_relation.sql
@@ -1,5 +1,5 @@
 ALTER TABLE data_quality_test_relations
-    ADD CONSTRAINT IF NOT EXIST dqt_relation_dataset_oddrn_fk FOREIGN KEY (dataset_oddrn) REFERENCES data_entity (oddrn);
+    ADD CONSTRAINT dqt_relation_dataset_oddrn_fk FOREIGN KEY (dataset_oddrn) REFERENCES data_entity (oddrn);
 
 ALTER TABLE data_quality_test_relations
-    ADD CONSTRAINT IF NOT EXIST dqt_relation_data_quality_test_oddrn_fk FOREIGN KEY (data_quality_test_oddrn) REFERENCES data_entity (oddrn)
+    ADD CONSTRAINT dqt_relation_data_quality_test_oddrn_fk FOREIGN KEY (data_quality_test_oddrn) REFERENCES data_entity (oddrn)
