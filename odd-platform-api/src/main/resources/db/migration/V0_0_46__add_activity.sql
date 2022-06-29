dfff5f4b79a851dfc5036d83b8e7fadf1ba6806f
dabdullin <abdul952008@gmail.com>
2022-06-29 17:50:47 +0300

    fix create table

--- odd-platform-api/src/main/resources/db/migration/V0_0_46__add_activity.sql
+++ odd-platform-api/src/main/resources/db/migration/V0_0_46__add_activity.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS activity
 (
-    data_entity_id bigint       NOT NULL,
-    event_type     varchar(50)  NOT NULL,
+    id             bigserial   not null,
+    data_entity_id bigint      NOT NULL,
+    event_type     varchar(50) NOT NULL,
     old_state      jsonb,
     new_state      jsonb,
-    created_at     timestamp    NOT NULL DEFAULT NOW(),
+    created_at     timestamp   NOT NULL DEFAULT NOW(),
     created_by     varchar(512),
-
+    PRIMARY KEY (id, created_at),
     CONSTRAINT activity_data_entity_id_fk FOREIGN KEY (data_entity_id) REFERENCES data_entity (id)
-) PARTITION BY RANGE ((date_trunc('day', created_at)));
+) PARTITION BY RANGE (created_at);
 
-CREATE INDEX ON activity (created_at);
+CREATE INDEX IF NOT EXISTS activity_created_at_idx ON activity (created_at);
