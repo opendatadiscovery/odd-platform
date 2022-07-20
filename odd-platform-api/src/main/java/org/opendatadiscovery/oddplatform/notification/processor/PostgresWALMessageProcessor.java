529c91de1ec31ec19bcb5ebde626ca44e79b7b9b
Nikita Dementev <ndementev@provectus.com>
2022-07-20 11:38:51 +0300

    Remove unnecessary exceptions

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/notification/processor/PostgresWALMessageProcessor.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/notification/processor/PostgresWALMessageProcessor.java
@@ -1,10 +1,7 @@
 package org.opendatadiscovery.oddplatform.notification.processor;
 
-import java.io.NotSerializableException;
 import org.opendatadiscovery.oddplatform.notification.dto.DecodedWALMessage;
-import org.opendatadiscovery.oddplatform.notification.exception.NotificationSenderException;
 
 public interface PostgresWALMessageProcessor {
-    void process(final DecodedWALMessage message)
-        throws InterruptedException, NotificationSenderException, NotSerializableException;
+    void process(final DecodedWALMessage message) throws InterruptedException;
 }
