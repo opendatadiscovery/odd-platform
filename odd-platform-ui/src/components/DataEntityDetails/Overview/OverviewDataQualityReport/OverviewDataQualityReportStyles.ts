743222aafab8ad85eb2bf2f1dadb482430644a0b
nikitosinar <nrotar@provectus.com>
2022-08-12 12:08:49 +0300

    Fix status report calculation

--- odd-platform-ui/src/components/DataEntityDetails/Overview/OverviewDataQualityReport/OverviewDataQualityReportStyles.ts
+++ odd-platform-ui/src/components/DataEntityDetails/Overview/OverviewDataQualityReport/OverviewDataQualityReportStyles.ts
@@ -27,44 +27,18 @@ export const CountLabel = styled(Typography)<CountLabelProps>(
 
 export const Bar = styled('div')<BarProps>(
   ({ theme, $testRunStatus, $testReport }) => {
-    const succRelation =
-      ($testReport?.successTotal || 0) / ($testReport?.total || 1);
-    const otherStatusesAdjustment =
-      200 / (Math.round(1 - succRelation) + 1);
+    const key =
+      `${$testRunStatus.toLowerCase()}Total` as keyof DataSetTestReport;
 
-    const calculateSuccessMaxWidth = (
-      totalPercentTestReport: number
-    ): string =>
-      `${
-        (totalPercentTestReport * 200) /
-        (Math.round(totalPercentTestReport) + 1)
-      }%`;
-    const calculateOtherStatusesMaxWidth = (
-      testRunStatus: DataEntityRunStatus,
-      adjustment: number,
-      testReport?: DataSetTestReport
-    ): number => {
-      const keyTestRunStatus =
-        `${testRunStatus.toLowerCase()}Total` as keyof DataSetTestReport;
-      const runStatusTotalTestReport =
-        (testReport && testReport[keyTestRunStatus]) || 0;
-      const totalPercentTestReport =
-        runStatusTotalTestReport / (testReport?.total || 1);
-      return totalPercentTestReport * adjustment;
-    };
-    const calculatedMaxWidth =
-      $testRunStatus === DataEntityRunStatus.SUCCESS
-        ? calculateSuccessMaxWidth(succRelation)
-        : `${calculateOtherStatusesMaxWidth(
-            $testRunStatus,
-            otherStatusesAdjustment,
-            $testReport
-          )}%`;
+    const relation = Math.round(
+      (100 * ($testReport?.[key] || 0)) / ($testReport?.total || 1)
+    );
+
     return {
       backgroundColor: theme.palette.runStatus[$testRunStatus],
       height: '8px',
       width: '100%',
-      maxWidth: calculatedMaxWidth,
+      maxWidth: `${relation}%`,
     };
   }
 );
