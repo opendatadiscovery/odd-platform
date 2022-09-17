1e9fda5c6dcf452af515c418a50b4a56fe7c7885
Ilgam Shakirov <ishakirov@provectus.com>
2022-09-18 00:38:17 +0300

    Add basic configuration for ESLint linter

---
+++ tests/.eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+};
