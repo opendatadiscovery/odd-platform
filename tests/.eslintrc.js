23ffec6db1283804c854099303c28d7498eb2424
Ilgam Shakirov <ishakirov@provectus.com>
2022-09-19 01:51:38 +0300

    Turns off all rules that might conflict with Prettier
    More info: https://github.com/prettier/eslint-config-prettier/

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -16,5 +16,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier' /** Make sure to put it last, so it gets the chance to override other configs */,
   ],
+  rules: {
+    /**
+     * After modifying this section, run the CLI helper tool to find problems.
+     * https://github.com/prettier/eslint-config-prettier/#cli-helper-tool
+     * While the "prettier" config can disable problematic rules in
+     * "some-other-config-you-use", it cannot touch "rules"!
+     * (That’s how ESLint works – it lets you override configs you extend.)
+     * The CLI helper tool reports that conflicts with Prettier, so you can remove it.
+     */
+  },
 };
