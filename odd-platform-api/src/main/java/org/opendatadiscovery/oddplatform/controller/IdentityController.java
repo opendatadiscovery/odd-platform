84683c91ce4f2a1079bf5a10e7733b7ff0cd19d5
Damir Abdullin <abdul952008@gmail.com>
2022-09-20 20:24:09 +0300

    Add dummy identity (#976)

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/controller/IdentityController.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/controller/IdentityController.java
@@ -1,10 +1,15 @@
 package org.opendatadiscovery.oddplatform.controller;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.opendatadiscovery.oddplatform.api.contract.api.IdentityApi;
+import org.opendatadiscovery.oddplatform.api.contract.model.Actions;
 import org.opendatadiscovery.oddplatform.api.contract.model.AssociatedOwner;
+import org.opendatadiscovery.oddplatform.api.contract.model.Identity;
+import org.opendatadiscovery.oddplatform.api.contract.model.Permission;
 import org.opendatadiscovery.oddplatform.service.IdentityService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
@@ -20,6 +25,13 @@ public class IdentityController implements IdentityApi {
     public Mono<ResponseEntity<AssociatedOwner>> whoami(final ServerWebExchange exchange) {
         return identityService.whoami()
             .map(ResponseEntity::ok)
-            .switchIfEmpty(Mono.just(ResponseEntity.noContent().build()));
+            .switchIfEmpty(Mono.just(new ResponseEntity<>(dummyOwner(), HttpStatus.OK)));
+    }
+
+    private AssociatedOwner dummyOwner() {
+        return new AssociatedOwner().identity(
+            new Identity().username("admin").actions(
+                new Actions().allowed(Arrays.asList(Permission.values()))
+            ));
     }
 }
