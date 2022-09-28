35bccaedf7161018e4a94c55aaa0e0be3ce9e3dd
Nikita Dementev <ndementev@provectus.com>
2022-09-29 00:28:23 +0300

    Fix broken tests

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/repository/reactive/ReactiveDataEntityTaskRunRepository.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/repository/reactive/ReactiveDataEntityTaskRunRepository.java
@@ -3,7 +3,9 @@ package org.opendatadiscovery.oddplatform.repository.reactive;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityRunStatus;
 import org.opendatadiscovery.oddplatform.model.tables.pojos.DataEntityTaskRunPojo;
+import org.opendatadiscovery.oddplatform.utils.Page;
 import reactor.core.publisher.Mono;
 
 public interface ReactiveDataEntityTaskRunRepository {
@@ -14,4 +16,9 @@ public interface ReactiveDataEntityTaskRunRepository {
     Mono<Void> bulkUpdate(final Collection<DataEntityTaskRunPojo> pojos);
 
     Mono<Void> insertLastRuns(final Collection<DataEntityTaskRunPojo> pojos);
+
+    Mono<Page<DataEntityTaskRunPojo>> getDataEntityRuns(final long dataQualityTestId,
+                                                        final DataEntityRunStatus status,
+                                                        final int page,
+                                                        final int size);
 }
--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/repository/reactive/ReactiveDataEntityTaskRunRepositoryImpl.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/repository/reactive/ReactiveDataEntityTaskRunRepositoryImpl.java
@@ -1,28 +1,37 @@
 package org.opendatadiscovery.oddplatform.repository.reactive;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.InsertSetStep;
+import org.jooq.Record;
 import org.jooq.Record1;
+import org.jooq.Select;
+import org.jooq.SelectConditionStep;
+import org.jooq.SortOrder;
 import org.jooq.Table;
 import org.jooq.impl.DSL;
 import org.opendatadiscovery.oddplatform.annotation.ReactiveTransactional;
+import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityRunStatus;
 import org.opendatadiscovery.oddplatform.model.tables.pojos.DataEntityTaskLastRunPojo;
 import org.opendatadiscovery.oddplatform.model.tables.pojos.DataEntityTaskRunPojo;
 import org.opendatadiscovery.oddplatform.model.tables.records.DataEntityTaskLastRunRecord;
 import org.opendatadiscovery.oddplatform.model.tables.records.DataEntityTaskRunRecord;
 import org.opendatadiscovery.oddplatform.repository.util.JooqQueryHelper;
 import org.opendatadiscovery.oddplatform.repository.util.JooqReactiveOperations;
+import org.opendatadiscovery.oddplatform.utils.Page;
 import org.opendatadiscovery.oddplatform.utils.Pair;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 import static java.util.function.Function.identity;
+import static org.opendatadiscovery.oddplatform.model.Tables.DATA_ENTITY;
 import static org.opendatadiscovery.oddplatform.model.Tables.DATA_ENTITY_TASK_LAST_RUN;
 import static org.opendatadiscovery.oddplatform.model.Tables.DATA_ENTITY_TASK_RUN;
 
@@ -34,8 +43,6 @@ public class ReactiveDataEntityTaskRunRepositoryImpl implements ReactiveDataEnti
 
     @Override
     public Mono<Map<String, Boolean>> existsByOddrns(final List<String> oddrns) {
-        // TODO: partitioned select?
-        // TODO: rewrite using exists?
         final var selectQuery = DSL.select(DATA_ENTITY_TASK_RUN.ODDRN)
             .from(DATA_ENTITY_TASK_RUN)
             .where(DATA_ENTITY_TASK_RUN.ODDRN.in(oddrns));
@@ -46,7 +53,6 @@ public class ReactiveDataEntityTaskRunRepositoryImpl implements ReactiveDataEnti
             .map(existingOddrns -> oddrns.stream().collect(Collectors.toMap(identity(), existingOddrns::contains)));
     }
 
-    // TODO: use from abstract?
     @Override
     public Mono<Void> bulkCreate(final Collection<DataEntityTaskRunPojo> pojos) {
         final List<DataEntityTaskRunRecord> records = pojos.stream()
@@ -64,7 +70,6 @@ public class ReactiveDataEntityTaskRunRepositoryImpl implements ReactiveDataEnti
         });
     }
 
-    // TODO: use from abstract?
     @Override
     public Mono<Void> bulkUpdate(final Collection<DataEntityTaskRunPojo> pojos) {
         final List<DataEntityTaskRunRecord> records = pojos.stream()
@@ -83,14 +88,12 @@ public class ReactiveDataEntityTaskRunRepositoryImpl implements ReactiveDataEnti
                 .set(fields)
                 .from(table)
                 .where(DATA_ENTITY_TASK_RUN.ID.eq(table.field(DATA_ENTITY_TASK_RUN.ID.getName(), Long.class)))
-                // TODO: remove returning
                 .returning();
 
             return jooqReactiveOperations.flux(query);
         }).then();
     }
 
-    // TODO: extract business logic to service/handler
     @Override
     @ReactiveTransactional
     public Mono<Void> insertLastRuns(final Collection<DataEntityTaskRunPojo> pojos) {
@@ -130,7 +133,6 @@ public class ReactiveDataEntityTaskRunRepositoryImpl implements ReactiveDataEnti
                     .map(e -> jooqReactiveOperations.newRecord(DATA_ENTITY_TASK_LAST_RUN, e))
                     .toList();
 
-                // TODO: partition somehow (onDuplicateKeyUpdate is not supported)
                 var insertStep = DSL.insertInto(DATA_ENTITY_TASK_LAST_RUN);
 
                 for (int i = 0; i < records.size() - 1; i++) {
@@ -153,4 +155,45 @@ public class ReactiveDataEntityTaskRunRepositoryImpl implements ReactiveDataEnti
             })
             .then();
     }
+
+    @Override
+    public Mono<Page<DataEntityTaskRunPojo>> getDataEntityRuns(final long dataQualityTestId,
+                                                               final DataEntityRunStatus status, final int page,
+                                                               final int size) {
+        final List<Condition> conditions = new ArrayList<>();
+        conditions.add(DATA_ENTITY.ID.eq(dataQualityTestId));
+        conditions.add(DATA_ENTITY.HOLLOW.isFalse());
+        if (status != null) {
+            conditions.add(DATA_ENTITY_TASK_RUN.STATUS.eq(status.name()));
+        }
+
+        final SelectConditionStep<Record> baseQuery = DSL
+            .select(DATA_ENTITY_TASK_RUN.fields())
+            .from(DATA_ENTITY_TASK_RUN)
+            .join(DATA_ENTITY).on(DATA_ENTITY.ODDRN.eq(DATA_ENTITY_TASK_RUN.TASK_ODDRN))
+            .where(conditions);
+
+        final Select<? extends Record> query = jooqQueryHelper.paginate(
+            baseQuery,
+            DATA_ENTITY_TASK_RUN.END_TIME,
+            SortOrder.DESC,
+            (page - 1) * size,
+            size
+        );
+
+        return jooqReactiveOperations.flux(query)
+            .collectList()
+            .flatMap(list -> jooqQueryHelper.pageifyResult(
+                list,
+                r -> r.into(DataEntityTaskRunPojo.class),
+                fetchCount(baseQuery)
+            ));
+    }
+
+    private Mono<Long> fetchCount(final Select<Record> query) {
+        return jooqReactiveOperations.mono(DSL.selectCount().from(query))
+            .map(Record1::value1)
+            .map(Long::valueOf);
+    }
+
 }
