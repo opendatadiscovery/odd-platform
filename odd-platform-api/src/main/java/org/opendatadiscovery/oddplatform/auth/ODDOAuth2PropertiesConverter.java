2373e11031cb144c61e21b45346148bef9647151
dabdullin <abdul952008@gmail.com>
2022-09-30 10:20:41 +0300

    don't set empty parameters

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/auth/ODDOAuth2PropertiesConverter.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/auth/ODDOAuth2PropertiesConverter.java
@@ -1,6 +1,8 @@
 package org.opendatadiscovery.oddplatform.auth;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 
 public final class ODDOAuth2PropertiesConverter {
 
@@ -13,18 +15,35 @@ public final class ODDOAuth2PropertiesConverter {
             final OAuth2ClientProperties.Registration registration = new OAuth2ClientProperties.Registration();
             registration.setClientId(provider.getClientId());
             registration.setClientSecret(provider.getClientSecret());
-            registration.setClientName(provider.getClientName());
+            if (StringUtils.isNotEmpty(provider.getClientName())) {
+                registration.setClientName(provider.getClientName());
+            }
             registration.setScope(provider.getScope());
-            registration.setRedirectUri(provider.getRedirectUri());
+            if (StringUtils.isNotEmpty(provider.getRedirectUri())) {
+                registration.setRedirectUri(provider.getRedirectUri());
+            }
+            registration.setAuthorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE.getValue());
             result.getRegistration().put(key, registration);
 
             final OAuth2ClientProperties.Provider clientProvider = new OAuth2ClientProperties.Provider();
-            clientProvider.setAuthorizationUri(provider.getAuthorizationUri());
-            clientProvider.setIssuerUri(provider.getIssuerUri());
-            clientProvider.setJwkSetUri(provider.getJwkSetUri());
-            clientProvider.setTokenUri(provider.getTokenUri());
-            clientProvider.setUserInfoUri(provider.getUserInfoUri());
-            clientProvider.setUserNameAttribute(provider.getUserNameAttribute());
+            if (StringUtils.isNotEmpty(provider.getAuthorizationUri())) {
+                clientProvider.setAuthorizationUri(provider.getAuthorizationUri());
+            }
+            if (StringUtils.isNotEmpty(provider.getIssuerUri())) {
+                clientProvider.setIssuerUri(provider.getIssuerUri());
+            }
+            if (StringUtils.isNotEmpty(provider.getJwkSetUri())) {
+                clientProvider.setJwkSetUri(provider.getJwkSetUri());
+            }
+            if (StringUtils.isNotEmpty(provider.getTokenUri())) {
+                clientProvider.setTokenUri(provider.getTokenUri());
+            }
+            if (StringUtils.isNotEmpty(provider.getUserInfoUri())) {
+                clientProvider.setUserInfoUri(provider.getUserInfoUri());
+            }
+            if (StringUtils.isNotEmpty(provider.getUserNameAttribute())) {
+                clientProvider.setUserNameAttribute(provider.getUserNameAttribute());
+            }
             result.getProvider().put(key, clientProvider);
         });
         return result;
