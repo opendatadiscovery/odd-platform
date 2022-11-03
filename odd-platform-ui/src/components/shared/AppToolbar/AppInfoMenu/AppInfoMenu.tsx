0afc4ff92b504b64116401119f9ad6434598dad3
alexeykozyurov <akozyurov@provectus.com>
2022-11-03 20:38:26 +0300

    Implemented app info menu.

---
+++ odd-platform-ui/src/components/shared/AppToolbar/AppInfoMenu/AppInfoMenu.tsx
@@ -0,0 +1,90 @@
+import React, { MouseEvent } from 'react';
+import { AppIconButton, AppMenu } from 'components/shared/index';
+import {
+  GitBookIcon,
+  GitHubIcon,
+  InformationIcon,
+  SlackIcon,
+} from 'components/shared/Icons';
+import { useAppSelector } from 'redux/lib/hooks';
+import { getVersion } from 'redux/selectors';
+import { Grid, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import * as S from './AppInfoMenuStyles';
+
+const AppInfoMenu: React.FC = () => {
+  const version = useAppSelector(getVersion);
+
+  const gitbookLink = 'https://docs.opendatadiscovery.org/';
+  const slackLink =
+    'https://opendatadiscovery.slack.com/join/shared_invite/zt-xqs5etsh-tytjLsAJK7uX3JSYdqreyg';
+  const githubLink = 'https://github.com/opendatadiscovery/odd-platform';
+
+  const menuId = 'app-info-menu';
+  const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleAppMenuOpen = (event: MouseEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleAppMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Grid>
+      <Grid item>
+        <AppIconButton
+          sx={{ mr: 2 }}
+          icon={<InformationIcon variant='dark' width={20} height={20} />}
+          color='unfilled'
+          edge='end'
+          aria-label='app info menu'
+          aria-controls={menuId}
+          aria-haspopup='true'
+          onClick={handleAppMenuOpen}
+          onMouseEnter={handleAppMenuOpen}
+        />
+      </Grid>
+      <AppMenu
+        MenuListProps={{ sx: { px: 1, py: 2 } }}
+        // sty={{pap}}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{ vertical: -20, horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={handleAppMenuClose}
+      >
+        <Link to={{ pathname: gitbookLink }} target='_blank'>
+          <S.MenuItem container onClick={handleAppMenuClose}>
+            <S.Icon>
+              <GitBookIcon />
+            </S.Icon>
+            <Typography variant='h3'>Documents</Typography>
+          </S.MenuItem>
+        </Link>
+        <Link to={{ pathname: slackLink }} target='_blank'>
+          <S.MenuItem container onClick={handleAppMenuClose}>
+            <S.Icon>
+              <SlackIcon />
+            </S.Icon>
+            <Typography variant='h3'>Slack</Typography>
+          </S.MenuItem>
+        </Link>
+        <Link to={{ pathname: githubLink }} target='_blank'>
+          <S.MenuItem container onClick={handleAppMenuClose}>
+            <S.Icon>
+              <GitHubIcon />
+            </S.Icon>
+            <Typography variant='h3'>{version}</Typography>
+          </S.MenuItem>
+        </Link>
+      </AppMenu>
+    </Grid>
+  );
+};
+
+export default AppInfoMenu;
--- odd-platform-ui/src/components/shared/Icons/InformationIcon.tsx
+++ odd-platform-ui/src/components/shared/Icons/InformationIcon.tsx
@@ -12,22 +12,34 @@ const styles = ({ theme }: { theme?: Theme }) => ({
   },
 });
 
-const InformationIcon: React.FC<SvgIconProps> = React.forwardRef(
-  ({ sx, ...props }, ref) => (
+const InformationIcon: React.FC<SvgIconProps & { variant?: 'light' | 'dark' }> =
+  React.forwardRef(({ sx, variant = 'light', ...props }, ref) => (
     <AppSvgIcon ref={ref} sx={sx} viewBox='0 0 16 16' {...props}>
-      <svg fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <circle cx='8' cy='8' r='7.5' stroke='currentColor' />
+      <svg
+        fill={variant === 'light' ? 'none' : 'black'}
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <circle
+          cx='8'
+          cy='8'
+          r='7.5'
+          stroke={variant === 'light' ? 'currentColor' : 'white'}
+        />
         <path
           d='M8 8L8 12'
-          stroke='currentColor'
+          stroke={variant === 'light' ? 'currentColor' : 'white'}
           strokeWidth='2'
           strokeLinecap='round'
           strokeLinejoin='round'
         />
-        <circle cx='8' cy='4' r='1' fill='currentColor' />
+        <circle
+          cx='8'
+          cy='4'
+          r='1'
+          fill={variant === 'light' ? 'currentColor' : 'white'}
+        />
       </svg>
     </AppSvgIcon>
-  )
-);
+  ));
 
 export default styled(InformationIcon)(styles);
