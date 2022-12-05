1a6ec67ec2fd0aebb34cc380919808ee02886e61
Flyspot <work.samoilov.pavel@gmail.com>
2022-12-05 15:45:24 +0200

    fix typo

--- tests/hooks/global-setup.ts
+++ tests/hooks/global-setup.ts
@@ -7,17 +7,17 @@ import path from 'path';
 import { configuration } from '../config/configuration';
 
 const makeDirIfNotExists = dir => {
-    if (!fs.existsSync(dir)) {
-        console.log(`creating ${dir} directory..`);
-        fs.mkdirSync(path.resolve(dir), { recursive: true });
-        console.log(`directory ${path.resolve(dir)} created: `, fs.existsSync(path.resolve(dir)));
-    }
+  if (!fs.existsSync(dir)) {
+    console.log(`creating ${dir} directory..`);
+    fs.mkdirSync(path.resolve(dir), { recursive: true });
+    console.log(`directory ${path.resolve(dir)} created: `, fs.existsSync(path.resolve(dir)));
+  }
 };
 
 const setup = (config: FullConfig): void => {
-    makeDirIfNotExists('./dist');
-    makeDirIfNotExists('./dist/workers');
-    console.log(`playwright checks: 
+  makeDirIfNotExists('./dist');
+  makeDirIfNotExists('./dist/workers');
+  console.log(`playwright checks: 
       directory to run: ${config.rootDir}
       tests to run: ${config.grep}
       environment: ${configuration.environment}
