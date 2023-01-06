49c5df7b3086ce1b6055cd7b42ab3cd22bafe762
dabdullin <abdul952008@gmail.com>
2023-01-06 16:50:39 +0300

    read groups claim from token

---
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/auth/handler/impl/AbstractOIDCUserHandler.java
@@ -0,0 +1,67 @@
+package org.opendatadiscovery.oddplatform.auth.handler.impl;
+
+import com.nimbusds.jose.shaded.json.JSONArray;
+import java.util.Objects;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.opendatadiscovery.oddplatform.auth.ODDOAuth2Properties;
+import org.opendatadiscovery.oddplatform.auth.handler.OAuthUserHandler;
+import org.opendatadiscovery.oddplatform.auth.mapper.GrantedAuthorityExtractor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import reactor.core.publisher.Mono;
+
+import static org.opendatadiscovery.oddplatform.utils.OperationUtils.containsIgnoreCase;
+
+@RequiredArgsConstructor
+public abstract class AbstractOIDCUserHandler implements OAuthUserHandler<OidcUser, OidcUserRequest> {
+    private final ODDOAuth2Properties oAuth2Properties;
+    private final GrantedAuthorityExtractor authorityExtractor;
+
+    @Override
+    public Mono<OidcUser> enrichUserWithProviderInformation(final OidcUser oidcUser, final OidcUserRequest request) {
+        final String registrationId = request.getClientRegistration().getRegistrationId();
+        final ODDOAuth2Properties.OAuth2Provider provider = oAuth2Properties.getClient().get(registrationId);
+        final String userNameAttributeName = provider.getUserNameAttribute();
+        final String userNameAttribute =
+            Objects.requireNonNullElse(userNameAttributeName, getDefaultUsernameAttribute());
+        boolean isAdmin = false;
+        final OidcIdToken token = oidcUser.getIdToken();
+        if (CollectionUtils.isNotEmpty(provider.getAdminPrincipals())) {
+            final String adminPrincipalAttribute = StringUtils.isNotEmpty(provider.getAdminAttribute())
+                ? provider.getAdminAttribute() : userNameAttributeName;
+            final String adminAttribute = token.getClaim(adminPrincipalAttribute);
+            final boolean containsUsername = containsIgnoreCase(provider.getAdminPrincipals(), adminAttribute);
+            if (containsUsername) {
+                isAdmin = true;
+            }
+        }
+        final String groupsClaim = StringUtils.isNotEmpty(provider.getGroupsClaim())
+            ? provider.getGroupsClaim() : getDefaultGroupsClaim();
+        if (StringUtils.isNotEmpty(groupsClaim) && CollectionUtils.isNotEmpty(provider.getAdminGroups())) {
+            final JSONArray groups = token.getClaim(groupsClaim);
+            if (groups != null) {
+                final boolean containsGroup = groups.stream()
+                    .filter(String.class::isInstance)
+                    .map(String.class::cast)
+                    .anyMatch(g -> containsIgnoreCase(provider.getAdminGroups(), g));
+                if (containsGroup) {
+                    isAdmin = true;
+                }
+            }
+        }
+        final Set<GrantedAuthority> authorities = authorityExtractor.getAuthorities(isAdmin);
+        final DefaultOidcUser enrichedUser =
+            new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo(), userNameAttribute);
+        return Mono.just(enrichedUser);
+    }
+
+    protected abstract String getDefaultUsernameAttribute();
+
+    protected abstract String getDefaultGroupsClaim();
+}
