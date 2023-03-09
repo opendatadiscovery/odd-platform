dbde798beb62ff53e2e1aa4678faeb91f1a9029d
Damir Abdullin <abdul952008@gmail.com>
2023-03-09 18:01:20 +0300

    Metrics bugfix, tests enhancement (#1268)

--- odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/api/ingestion/MetricsIngestionTest.java
+++ odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/api/ingestion/MetricsIngestionTest.java
@@ -68,17 +68,26 @@ public class MetricsIngestionTest extends BaseIngestionTest {
         assertMetrics(foundEntityId, expectedMetricSet, false);
 
         // Update metrics timestamp to value more than previous
+        // this metric point should be updated
         final MetricSetList newMetrics = createMetrics(datasetToIngest.getOddrn(), "metrics/gauge_and_count.json");
-        final MetricPoint newMetricPointToUpdate = newMetrics.getItems().get(0).getMetricFamilies().get(0)
+        final MetricPoint firstMetricPointToUpdate = newMetrics.getItems().get(0).getMetricFamilies().get(0)
             .getMetrics().get(0)
             .getMetricPoints().get(0);
-        newMetricPointToUpdate.setTimestamp(newMetricPointToUpdate.getTimestamp() + 100);
-        newMetricPointToUpdate.getGaugeValue().setValue(BigDecimal.ONE);
-        ingestMetrics(newMetrics);
+        firstMetricPointToUpdate.setTimestamp(firstMetricPointToUpdate.getTimestamp() + 100);
+        firstMetricPointToUpdate.getGaugeValue().setValue(BigDecimal.ONE);
 
         // We expect, that metric value is updated
         final org.opendatadiscovery.oddplatform.api.contract.model.MetricSet newExpectedMetricSet =
             buildExpectedMetricSet(newMetrics.getItems().get(0));
+
+        // this metric point shouldn't be updated
+        final MetricPoint secondMetricPointToUpdate = newMetrics.getItems().get(0).getMetricFamilies().get(0)
+            .getMetrics().get(1)
+            .getMetricPoints().get(0);
+        secondMetricPointToUpdate.setTimestamp(secondMetricPointToUpdate.getTimestamp() - 100);
+        secondMetricPointToUpdate.getGaugeValue().setValue(BigDecimal.ONE);
+        ingestMetrics(newMetrics);
+
         assertMetrics(foundEntityId, newExpectedMetricSet, false);
     }
 
@@ -110,7 +119,7 @@ public class MetricsIngestionTest extends BaseIngestionTest {
             "metrics/histogram_and_summary.json");
         final MetricPoint metricPointToUpdate = updatedMetricSet.getItems().get(0).getMetricFamilies().get(0)
             .getMetrics().get(0).getMetricPoints().get(0);
-        metricPointToUpdate.setTimestamp(metricPointToUpdate.getTimestamp() - 100);
+        metricPointToUpdate.setTimestamp(metricPointToUpdate.getTimestamp() - 1000);
         metricPointToUpdate.getHistogramValue().setBuckets(
             List.of(
                 new Bucket().count(5L).upperBound(new BigDecimal(10)),
@@ -125,21 +134,35 @@ public class MetricsIngestionTest extends BaseIngestionTest {
         // Update metrics timestamp to value more than previous
         final MetricSetList newUpdatedMetricSet = createMetrics(datasetToIngest.getOddrn(),
             "metrics/histogram_and_summary.json");
-        final MetricPoint newUpdatedMetricPoint = newUpdatedMetricSet.getItems().get(0).getMetricFamilies().get(0)
+        final MetricPoint firstUpdatedMetricPoint = newUpdatedMetricSet.getItems().get(0).getMetricFamilies().get(0)
             .getMetrics().get(0).getMetricPoints().get(0);
-        newUpdatedMetricPoint.setTimestamp(newUpdatedMetricPoint.getTimestamp() + 100);
-        newUpdatedMetricPoint.getHistogramValue().setCount(null);
-        newUpdatedMetricPoint.getHistogramValue().setBuckets(
+        firstUpdatedMetricPoint.setTimestamp(firstUpdatedMetricPoint.getTimestamp() + 100);
+        firstUpdatedMetricPoint.getHistogramValue().setCount(null);
+        firstUpdatedMetricPoint.getHistogramValue().setBuckets(
             List.of(
+                new Bucket().count(35L).upperBound(new BigDecimal(10)),
                 new Bucket().count(6L).upperBound(new BigDecimal(30)),
                 new Bucket().count(16L).upperBound(new BigDecimal(40))
             )
         );
-        ingestMetrics(newUpdatedMetricSet);
 
         final org.opendatadiscovery.oddplatform.api.contract.model.MetricSet newExpectedMetricSet =
             buildExpectedMetricSet(newUpdatedMetricSet.getItems().get(0));
-        assertMetrics(foundEntityId, newExpectedMetricSet, false);
+
+        // This metric point should not be updated, because timestamp is less than previous one
+        final MetricPoint secondUpdatedMetricPoint = newUpdatedMetricSet.getItems().get(0).getMetricFamilies().get(0)
+            .getMetrics().get(1).getMetricPoints().get(0);
+        secondUpdatedMetricPoint.setTimestamp(secondUpdatedMetricPoint.getTimestamp() - 100);
+        secondUpdatedMetricPoint.getHistogramValue().setCount(null);
+        secondUpdatedMetricPoint.getHistogramValue().setBuckets(
+            List.of(
+                new Bucket().count(43L).upperBound(new BigDecimal(11)),
+                new Bucket().count(55L).upperBound(new BigDecimal(22)),
+                new Bucket().count(11L).upperBound(new BigDecimal(44))
+            )
+        );
+
+        ingestMetrics(newUpdatedMetricSet);
 
         // We expect, that metric value is updated
         assertMetrics(foundEntityId, newExpectedMetricSet, false);
