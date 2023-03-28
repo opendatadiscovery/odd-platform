abc73520d328e2aeaa72770b3532b282e86b8b3e
alexeykozyurov <akozyurov@provectus.com>
2023-03-28 21:07:06 +0300

    removed deg lineage

--- odd-platform-ui/src/components/DataEntityDetails/Lineage/DEGLineage/DEGLineage.tsx
+++ odd-platform-ui/src/components/DataEntityDetails/Lineage/DEGLineage/DEGLineage.tsx
@@ -4,10 +4,10 @@ import { useDataEntityGroupLineage } from 'lib/hooks/api';
 import { useAtom } from 'jotai';
 import { AppCircularProgress, EmptyContentPlaceholder } from 'components/shared';
 // import DEGLineageControls from './components/DEGLineageControls/DEGLineageControls';
-// import DEGLineageLayouter from './DEGLineageLayouter/DEGLineageLayouter';
+import DEGLineageLayouter from './DEGLineageLayouter/DEGLineageLayouter';
 import { isLayoutedAtom } from './lib/atoms';
 // import ZoomableDEGLineage from './ZoomableDEGLineage/ZoomableDEGLineage';
-// import * as S from './DEGLineage.styles';
+import * as S from './DEGLineage.styles';
 
 const DEGLineage: React.FC = () => {
   const { dataEntityId } = useAppParams();
@@ -24,26 +24,24 @@ const DEGLineage: React.FC = () => {
     return <EmptyContentPlaceholder />;
   }
 
-  return null;
-
-  // return (
-  //   <S.Container>
-  //     {(!isLayouted || isFetching || isLoading) && rawNodes.length !== 0 && (
-  //       <>
-  //         <S.LoaderContainer>
-  //           <AppCircularProgress size={16} text='Loading lineage' />
-  //         </S.LoaderContainer>
-  //         <DEGLineageLayouter nodes={rawNodes} edges={rawEdges} />
-  //       </>
-  //     )}
-  //     {isSuccess && isLayouted && (
-  //       <S.LineageViewContainer>
-  //         <DEGLineageControls />
-  //         <ZoomableDEGLineage />
-  //       </S.LineageViewContainer>
-  //     )}
-  //   </S.Container>
-  // );
+  return (
+    <S.Container>
+      {(!isLayouted || isFetching || isLoading) && rawNodes.length !== 0 && (
+        <>
+          <S.LoaderContainer>
+            <AppCircularProgress size={16} text='Loading lineage' />
+          </S.LoaderContainer>
+          <DEGLineageLayouter nodes={rawNodes} edges={rawEdges} />
+        </>
+      )}
+      {/* {isSuccess && isLayouted && ( */}
+      {/*   <S.LineageViewContainer> */}
+      {/*     <DEGLineageControls /> */}
+      {/*     <ZoomableDEGLineage /> */}
+      {/*   </S.LineageViewContainer> */}
+      {/* )} */}
+    </S.Container>
+  );
 };
 
 export default DEGLineage;
