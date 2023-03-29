be4f30b1cbd3d5e90055f270731a4379e85e7d86
alexeykozyurov <akozyurov@provectus.com>
2023-03-29 13:50:10 +0300

    updated config

--- odd-platform-ui/vite.config.ts
+++ odd-platform-ui/vite.config.ts
@@ -2,12 +2,12 @@ import {
   defineConfig,
   loadEnv,
   type UserConfigExport,
-  // splitVendorChunkPlugin,
-  // type Plugin,
-  // type PluginOption,
+  splitVendorChunkPlugin,
+  type Plugin,
+  type PluginOption,
 } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
 import dns from 'dns';
@@ -16,24 +16,24 @@ import dns from 'dns';
 
 dns.setDefaultResultOrder('verbatim');
 
-// interface SourcemapExclude {
-//   excludeNodeModules?: boolean;
-// }
-//
-// function sourcemapExclude(opts?: SourcemapExclude): Plugin {
-//   return {
-//     name: 'sourcemap-exclude',
-//     transform(code: string, id: string) {
-//       if (opts?.excludeNodeModules && id.includes('node_modules')) {
-//         return {
-//           code,
-//           // https://github.com/rollup/rollup/blob/master/docs/plugin-development/index.md#source-code-transformations
-//           map: null,
-//         };
-//       }
-//     },
-//   };
-// }
+interface SourcemapExclude {
+  excludeNodeModules?: boolean;
+}
+
+function sourcemapExclude(opts?: SourcemapExclude): Plugin {
+  return {
+    name: 'sourcemap-exclude',
+    transform(code: string, id: string) {
+      if (opts?.excludeNodeModules && id.includes('node_modules')) {
+        return {
+          code,
+          // https://github.com/rollup/rollup/blob/master/docs/plugin-development/index.md#source-code-transformations
+          map: null,
+        };
+      }
+    },
+  };
+}
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -55,9 +55,9 @@ export default defineConfig(({ mode }) => {
   const defaultPlugins = [
     react(),
     tsconfigPaths(),
-    // sourcemapExclude({ excludeNodeModules: true }),
-    // splitVendorChunkPlugin(),
-    // visualizer({ open: true }) as PluginOption,
+    sourcemapExclude({ excludeNodeModules: true }),
+    splitVendorChunkPlugin(),
+    visualizer({ open: true }) as PluginOption,
   ];
 
   const defaultConfig: UserConfigExport = {
@@ -68,27 +68,27 @@ export default defineConfig(({ mode }) => {
       // commonjsOptions: {
       //   sourceMap: false,
       // },
-      // rollupOptions: {
-      //   // maxParallelFileOps: Math.max(1, cpus().length - 1),
-      //   output: {
-      //     manualChunks: {
-      //       jsoneditor: ['vanilla-jsoneditor'],
-      //       elkjs: ['elkjs'],
-      //       elkjsBundled: ['elkjs/lib/elk.bundled'],
-      //     },
-      //     // manualChunks: id => {
-      //     //   if (id.includes('node_modules')) {
-      //     //     return 'vendor';
-      //     //   }
-      //     // },
-      //     sourcemapIgnoreList: relativeSourcePath => {
-      //       const normalizedPath = path.normalize(relativeSourcePath);
-      //       return normalizedPath.includes('node_modules');
-      //     },
-      //     sourcemap: false,
-      //   },
-      //   cache: false,
-      // },
+      rollupOptions: {
+        // maxParallelFileOps: Math.max(1, cpus().length - 1),
+        output: {
+          // manualChunks: {
+          //   jsoneditor: ['vanilla-jsoneditor'],
+          //   elkjs: ['elkjs'],
+          //   elkjsBundled: ['elkjs/lib/elk.bundled'],
+          // },
+          manualChunks: id => {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          // sourcemapIgnoreList: relativeSourcePath => {
+          //   const normalizedPath = path.normalize(relativeSourcePath);
+          //   return normalizedPath.includes('node_modules');
+          // },
+          sourcemap: false,
+        },
+        cache: false,
+      },
     },
     test: {
       globals: true,
