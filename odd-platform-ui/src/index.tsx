6208ac9b453b49d7c14e87b54e2f5a1905507631
alexeykozyurov <akozyurov@provectus.com>
2023-04-07 12:46:55 +0300

    tanstack query update

--- odd-platform-ui/src/index.tsx
+++ odd-platform-ui/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import {
   StyledEngineProvider,
