62f152a6fc08eaebbbf7f21b81ec5441ee485ba1
alexeykozyurov <akozyurov@provectus.com>
2023-04-12 21:45:54 +0300

    Restyled markdown copy button

--- odd-platform-ui/src/components/shared/Markdown/Markdown.tsx
+++ odd-platform-ui/src/components/shared/Markdown/Markdown.tsx
@@ -1,11 +1,25 @@
 import React, { type FC } from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import type { ReactMarkdownProps } from 'react-markdown/lib/ast-to-react';
+import CopyButton from 'components/shared/CopyButton/CopyButton';
 
 interface MarkdownProps {
   value: string;
   editor?: boolean;
   disableCopy?: boolean;
 }
+
+type MarkdownCopyButtonProps = Omit<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+  'ref'
+> &
+  ReactMarkdownProps;
+
+const MarkdownCopyButton = ({ node, ...props }: MarkdownCopyButtonProps) => (
+  <div {...props} style={{ backgroundColor: 'transparent' }}>
+    <CopyButton stringToCopy={node.properties?.dataCode as string} text='Copy' />
+  </div>
+);
 
 const Markdown: FC<MarkdownProps> = ({ value, editor = false, disableCopy = false }) => {
   const wrapperElement = { 'data-color-mode': 'light' } as {
@@ -24,6 +38,7 @@ const Markdown: FC<MarkdownProps> = ({ value, editor = false, disableCopy = fals
       style={{ width: '100%' }}
       wrapperElement={wrapperElement}
       disableCopy={disableCopy}
+      components={{ div: MarkdownCopyButton }}
     />
   );
 };
