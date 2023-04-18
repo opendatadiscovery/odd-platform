9e274dbe438d4cedf9deae31ffcf07a536076d2c
dabdullin <abdul952008@gmail.com>
2023-04-18 12:36:28 +0300

    Fix javadoc

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/DatasetVersionServiceImpl.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/DatasetVersionServiceImpl.java
@@ -150,7 +150,7 @@ public class DatasetVersionServiceImpl implements DatasetVersionService {
      * @param secondVersionFields dataset fields for the second version
      * @param changedPojoOddrns Dataset field oddrns, which we detected in previous iteration
      * @return dataset field oddrns, which parent field oddrn was changed and all field oddrns,
-     * which are hierarchically below
+     * which are hierarchically below.
      */
     private Set<String> getParentOddrnChangedPojos(final Map<String, DatasetFieldPojo> firstVersionFields,
                                                    final Map<String, DatasetFieldPojo> secondVersionFields,
