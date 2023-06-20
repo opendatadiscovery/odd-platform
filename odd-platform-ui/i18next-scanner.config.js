50b634fe0d3a2d92f101e576d3008296bde01a60
alexeykozyurov <akozyurov@provectus.com>
2023-06-20 13:53:08 +0300

    Translation packages config. Scanner config.

---
+++ odd-platform-ui/i18next-scanner.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  input: ['./tmp/**/*.{js,jsx}', '!./tmp/**/*.test.{js,jsx}', '!**/node_modules/**'],
+  output: './',
+  options: {
+    removeUnusedKeys: true,
+    sort: true,
+    func: {
+      list: ['i18next.t', 'i18n.t', 't', '__'],
+      extensions: ['.js', '.jsx'],
+    },
+    trans: {
+      component: 'Trans',
+      i18nKey: 'i18nKey',
+      defaultsKey: 'defaults',
+      extensions: ['.js', '.jsx'],
+      fallbackKey: false,
+    },
+    lngs: ['en', 'bg', 'fr', 'de'],
+    defaultLng: 'en',
+    defaultValue: '',
+    resource: {
+      loadPath: './src/locales/translations/{{lng}}.json',
+      savePath: './src/locales/translations/{{lng}}.json',
+      jsonIndent: 2,
+      lineEnding: '\n',
+    },
+    keySeparator: '.',
+    pluralSeparator: '_',
+    contextSeparator: '_',
+    contextDefaultValues: [],
+    interpolation: {
+      prefix: '{{',
+      suffix: '}}',
+    },
+  },
+};
---
+++ odd-platform-ui/src/locales/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const resources = {
+  en: { translation: {} },
+  de: { translation: {} },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: { escapeValue: false },
+    react: { useSuspense: false },
+  });
+
+export default i18n;
