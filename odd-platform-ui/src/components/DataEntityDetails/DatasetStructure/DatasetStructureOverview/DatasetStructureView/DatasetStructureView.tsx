c833532c2c62ec670b64a1c6c0e3838ad9dbd3ae
alexeykozyurov <akozyurov@provectus.com>
2023-07-10 13:41:22 +0300

    Structure layout fix.

--- odd-platform-ui/src/components/DataEntityDetails/DatasetStructure/DatasetStructureOverview/DatasetStructureView/DatasetStructureView.tsx
+++ odd-platform-ui/src/components/DataEntityDetails/DatasetStructure/DatasetStructureOverview/DatasetStructureView/DatasetStructureView.tsx
@@ -3,18 +3,19 @@ import { Grid } from '@mui/material';
 import DatasetStructureHeader from './DatasetStructureHeader/DatasetStructureHeader';
 import DatasetFieldOverview from './DatasetFieldOverview/DatasetFieldOverview';
 import DatasetStructureList from './DatasetStructureList/DatasetStructureList';
+import * as S from './DatasetStructureView.styles';
 
 const DatasetStructureView: FC = () => (
   <Grid container>
     <DatasetStructureHeader />
-    <Grid container sx={{ borderTop: '1px solid', borderTopColor: 'divider' }}>
-      <Grid item lg={6}>
+    <S.Container>
+      <S.Item>
         <DatasetStructureList />
-      </Grid>
-      <Grid item lg={6} sx={{ borderLeft: '1px solid', borderLeftColor: 'divider' }}>
+      </S.Item>
+      <S.Item>
         <DatasetFieldOverview />
-      </Grid>
-    </Grid>
+      </S.Item>
+    </S.Container>
   </Grid>
 );
 
