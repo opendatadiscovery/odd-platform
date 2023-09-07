910d5fa63fcd6f0cd9df999291c86f82a576ace9
alexeykozyurov <akozyurov@provectus.com>
2023-09-07 19:26:54 +0300

    fix

--- odd-platform-ui/src/locales/i18n.ts
+++ odd-platform-ui/src/locales/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './translations/en.json';
 import es from './translations/es.json';
 import ch from './translations/ch.json';
@@ -13,13 +12,10 @@ const resources = {
   fr: { translation: fr },
 };
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: ['en', 'es', 'ch', 'fr'],
-    debug: true,
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: ['en', 'es', 'ch', 'fr'],
+  debug: true,
+});
 
 export default i18n;
