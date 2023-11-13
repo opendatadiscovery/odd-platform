bdb0854f6540aef124f9612f5d118d3f678f2c36
vburlachenko <slaboezveno3@gmail.com>
2023-11-13 15:50:54 +0200

    1513 - Implementation of init test for Data Quality Dashboard

--- odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/repository/ReactiveDataQualityRunsRepositoryTest.java
+++ odd-platform-api/src/test/java/org/opendatadiscovery/oddplatform/repository/ReactiveDataQualityRunsRepositoryTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.IntStream;
-
 import org.jooq.JSONB;
 import org.jooq.impl.DSL;
 import org.junit.jupiter.api.BeforeAll;
