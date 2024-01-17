53408d634978f364e90a32ee1f3ef7bee8a2bbc1
Andrey Nenashev <anenashev@provectus.com>
2024-01-17 12:31:59 +0100

    bugfixes(fe): adjust query examples styles (#1583)

--- odd-platform-ui/src/components/DataModelling/QueryExampleSearchResults/QueryExamplesList.tsx
+++ odd-platform-ui/src/components/DataModelling/QueryExampleSearchResults/QueryExamplesList.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 import { useSearchQueryExamples } from 'lib/hooks/api/dataModelling/searchQueryExamples';
 import {
   EmptyContentPlaceholder,
+  QueryExamplesListHeader,
   QueryExamplesListItem,
   QueryExamplesSkeleton,
   ScrollableContainer,
@@ -29,22 +30,25 @@ const QueryExamplesList = () => {
   );
 
   return (
-    <ScrollableContainer container id='query-examples-list' $offsetY={165}>
-      <InfiniteScroll
-        dataLength={queryExamples.length}
-        next={fetchNextPage}
-        hasMore={hasNextPage}
-        loader={<QueryExamplesSkeleton />}
-        scrollThreshold='200px'
-        scrollableTarget='query-examples-list'
-      >
-        {queryExamples.map(qe => (
-          <QueryExamplesListItem queryExample={qe} key={qe.definition} />
-        ))}
-        {isLoading && <QueryExamplesSkeleton />}
-        {isEmpty && <EmptyContentPlaceholder offsetTop={215} />}
-      </InfiniteScroll>
-    </ScrollableContainer>
+    <>
+      <QueryExamplesListHeader />
+      <ScrollableContainer container id='query-examples-list' $offsetY={165}>
+        <InfiniteScroll
+          dataLength={queryExamples.length}
+          next={fetchNextPage}
+          hasMore={hasNextPage}
+          loader={<QueryExamplesSkeleton />}
+          scrollThreshold='200px'
+          scrollableTarget='query-examples-list'
+        >
+          {queryExamples.map(qe => (
+            <QueryExamplesListItem queryExample={qe} key={qe.definition} />
+          ))}
+          {isLoading && <QueryExamplesSkeleton />}
+          {isEmpty && <EmptyContentPlaceholder offsetTop={215} />}
+        </InfiniteScroll>
+      </ScrollableContainer>
+    </>
   );
 };
 
