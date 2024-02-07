b984d3136b201c90709c222877480090dbf6ff61
Andrey Nenashev <anenashev@provectus.com>
2024-02-07 14:40:50 +0100

    dependencies: update openapi-generator-cli (#1609)

--- odd-platform-ui/generate.sh
+++ odd-platform-ui/generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-GENERATOR_IMAGE="openapitools/openapi-generator-cli:v5.1.1"
+GENERATOR_IMAGE="openapitools/openapi-generator-cli:v7.2.0"
 CONFIG_PATH="odd-platform-ui/openapi-config.yaml"
 WORKDIR="//mnt" # Using double-slash for compatibility with Windows
 HOST_DIRECTORY=/$(pwd)/..
 
-docker run -v $HOST_DIRECTORY:$WORKDIR -w $WORKDIR --rm $GENERATOR_IMAGE generate -c $CONFIG_PATH
+docker run -v "$HOST_DIRECTORY:$WORKDIR" -w "$WORKDIR" --rm "$GENERATOR_IMAGE" generate -c "$CONFIG_PATH"
