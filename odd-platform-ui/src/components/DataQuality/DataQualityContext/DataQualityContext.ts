a77acec92e16c5ee3f8bccc4655f522ab8eb8f7d
ayemets-corcentric <106543964+ayemets-corcentric@users.noreply.github.com>
2024-02-08 23:07:58 +0100

    feat(odd-platform-ui): create dataquality context (#1590)

---
+++ odd-platform-ui/src/components/DataQuality/DataQualityContext/DataQualityContext.ts
@@ -0,0 +1,20 @@
+import React, { createContext, useContext } from 'react';
+import type { DataQualityRunsApiGetDataQualityTestsRunsRequest } from 'generated-sources/apis/DataQualityRunsApi';
+
+export interface DataQualityContextProps {
+  filterState: DataQualityRunsApiGetDataQualityTestsRunsRequest;
+  updateFilter: (
+    filterName: keyof DataQualityRunsApiGetDataQualityTestsRunsRequest,
+    value: Array<number>
+  ) => void;
+}
+
+const defaultBehaviour: DataQualityContextProps = {
+  filterState: {},
+  updateFilter: () => {},
+};
+
+export const DataQualityContext =
+  createContext<DataQualityContextProps>(defaultBehaviour);
+
+export const useDataQualityContext = () => useContext(DataQualityContext);
