a4e1b7c44ef751596d80d51353c3f1b78f81d611
ayemets-corcentric <106543964+ayemets-corcentric@users.noreply.github.com>
2024-02-13 10:28:29 +0100

    feat(odd-platform-ui): add initial data to  improve chart loading (#1590)

--- odd-platform-ui/src/lib/hooks/api/dataQuality.ts
+++ odd-platform-ui/src/lib/hooks/api/dataQuality.ts
@@ -1,6 +1,75 @@
 import { useQuery } from '@tanstack/react-query';
 import type { DataQualityRunsApiGetDataQualityTestsRunsRequest } from 'generated-sources/apis/DataQualityRunsApi';
 import { dataQualityRunsApi } from 'lib/api';
+import type { DataQualityResults } from 'generated-sources/models/DataQualityResults';
+import type { DataQualityRunStatusCount } from 'generated-sources/models/DataQualityRunStatusCount';
+
+const emptyTestResults: Array<DataQualityRunStatusCount> = [
+  {
+    status: 'SUCCESS',
+    count: 0,
+  },
+  {
+    status: 'FAILED',
+    count: 0,
+  },
+  {
+    status: 'SKIPPED',
+    count: 0,
+  },
+  {
+    status: 'BROKEN',
+    count: 0,
+  },
+  {
+    status: 'ABORTED',
+    count: 0,
+  },
+  {
+    status: 'UNKNOWN',
+    count: 0,
+  },
+];
+
+const initialData: DataQualityResults = {
+  testResults: [
+    {
+      category: 'Assertion Tests',
+      results: [...emptyTestResults],
+    },
+    {
+      category: 'Freshness Anomalies',
+      results: [...emptyTestResults],
+    },
+    {
+      category: 'Schema Changes',
+      results: [...emptyTestResults],
+    },
+    {
+      category: 'Volume Anomalies',
+      results: [...emptyTestResults],
+    },
+    {
+      category: 'Column Values Anomalies',
+      results: [...emptyTestResults],
+    },
+    {
+      category: 'Unknown category',
+      results: [...emptyTestResults],
+    },
+  ],
+  tablesDashboard: {
+    tablesHealth: {
+      healthyTables: 0,
+      errorTables: 0,
+      warningTables: 0,
+    },
+    monitoredTables: {
+      monitoredTables: 0,
+      notMonitoredTables: 0,
+    },
+  },
+};
 
 export function useGetDataQualityDashboard(
   params: DataQualityRunsApiGetDataQualityTestsRunsRequest
@@ -8,5 +77,6 @@ export function useGetDataQualityDashboard(
   return useQuery({
     queryKey: ['dataQualityDashboard', params],
     queryFn: async () => dataQualityRunsApi.getDataQualityTestsRuns(params),
+    initialData,
   });
 }
