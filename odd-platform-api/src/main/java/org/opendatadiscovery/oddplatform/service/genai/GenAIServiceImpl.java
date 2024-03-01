0be7f6d0d061c28d7eedf18bd8cd88fb2c995b03
vburlachenko <slaboezveno3@gmail.com>
2024-03-01 20:29:15 +0200

    1639 - fix sonar

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/genai/GenAIServiceImpl.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/genai/GenAIServiceImpl.java
@@ -4,15 +4,14 @@ import com.google.common.base.CharMatcher;
 import io.netty.handler.timeout.ReadTimeoutException;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.opendatadiscovery.oddplatform.api.contract.model.GenAIRequest;
 import org.opendatadiscovery.oddplatform.api.contract.model.GenAIResponse;
+import org.opendatadiscovery.oddplatform.config.properties.GenAIProperties;
 import org.opendatadiscovery.oddplatform.exception.BadUserRequestException;
 import org.opendatadiscovery.oddplatform.exception.GenAIException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -23,22 +22,19 @@ public class GenAIServiceImpl implements GenAIService {
     public static final String QUERY_DATA = "/query_data";
     public static final String QUESTION_FIELD = "question";
 
-    private final String genAIUrl;
-    private final Integer getAiRequestTimeout;
+    private final GenAIProperties genAIProperties;
     private final WebClient webClient;
 
     @Autowired
-    public GenAIServiceImpl(@Value("${genai.url:}") final String genAIUrl,
-                            @Value("${genai.request_timeout:2}") final Integer getAiRequestTimeout,
+    public GenAIServiceImpl(final GenAIProperties genAIProperties,
                             @Qualifier("genAiWebClient") final WebClient webClient) {
-        this.genAIUrl = genAIUrl;
-        this.getAiRequestTimeout = getAiRequestTimeout;
+        this.genAIProperties = genAIProperties;
         this.webClient = webClient;
     }
 
     @Override
     public Mono<GenAIResponse> getResponseFromGenAI(final GenAIRequest request) {
-        if (StringUtils.isBlank(genAIUrl)) {
+        if (!genAIProperties.isEnabled()) {
             return Mono.error(new BadUserRequestException("Gen AI is disabled"));
         }
 
@@ -51,7 +47,7 @@ public class GenAIServiceImpl implements GenAIService {
                 .body(StringEscapeUtils.unescapeJava(CharMatcher.is('\"').trimFrom(item))))
             .onErrorResume(e -> e.getCause() instanceof ReadTimeoutException
                 ? Mono.error(new GenAIException(
-                    "Gen AI request take longer that %s min".formatted(getAiRequestTimeout)))
+                    "Gen AI request take longer that %s min".formatted(genAIProperties.getRequestTimeout())))
                 : Mono.error(new GenAIException(e)));
     }
 }
