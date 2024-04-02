e220f4371af47db07026334af757e93f1ac44554
vburlachenko <slaboezveno3@gmail.com>
2024-04-02 17:42:36 +0300

    1650 - Added datasetField ID for ERD Relationship Details, DataEntityRef object instead of data_entity_id and dataset_oddrn fields,erdDetails and GraphDetails to RelationshipDetails entity.

--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/repository/reactive/ReactiveRelationshipsRepository.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/repository/reactive/ReactiveRelationshipsRepository.java
@@ -2,7 +2,7 @@ package org.opendatadiscovery.oddplatform.repository.reactive;
 
 import java.util.List;
 import org.opendatadiscovery.oddplatform.api.contract.model.RelationshipsType;
-import org.opendatadiscovery.oddplatform.dto.RelationshipDto;
+import org.opendatadiscovery.oddplatform.dto.RelationshipDetailsDto;
 import org.opendatadiscovery.oddplatform.model.tables.pojos.RelationshipsPojo;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,5 +11,8 @@ public interface ReactiveRelationshipsRepository extends ReactiveCRUDRepository<
 
     Mono<List<RelationshipsPojo>> getRelationshipByDataEntityIds(final List<Long> dataEntityRelationshipIds);
 
-    Flux<RelationshipDto> getRelationsByDatasetIdAndType(final Long dataEntityId, final RelationshipsType type);
+    Flux<RelationshipDetailsDto> getRelationsByDatasetIdAndType(final Long dataEntityId, final RelationshipsType type);
+
+    Mono<RelationshipDetailsDto> getRelationshipByIdAndType(Long relationshipId,
+                                                            RelationshipsType relationshipsType);
 }
--- odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/RelationshipsServiceImpl.java
+++ odd-platform-api/src/main/java/org/opendatadiscovery/oddplatform/service/RelationshipsServiceImpl.java
@@ -1,15 +1,13 @@
 package org.opendatadiscovery.oddplatform.service;
 
 import lombok.RequiredArgsConstructor;
-import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityERDRelationshipDetails;
-import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityGraphRelationshipDetails;
+import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityRelationshipDetails;
+import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityRelationshipDetailsList;
 import org.opendatadiscovery.oddplatform.api.contract.model.DataEntityRelationshipList;
 import org.opendatadiscovery.oddplatform.api.contract.model.RelationshipsType;
 import org.opendatadiscovery.oddplatform.exception.NotFoundException;
-import org.opendatadiscovery.oddplatform.mapper.RelationshipDetailsMapper;
 import org.opendatadiscovery.oddplatform.mapper.RelationshipMapper;
 import org.opendatadiscovery.oddplatform.repository.reactive.ReactiveDataEntityRelationshipRepository;
-import org.opendatadiscovery.oddplatform.repository.reactive.ReactiveRelationshipsDetailsRepository;
 import org.opendatadiscovery.oddplatform.repository.reactive.ReactiveRelationshipsRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -19,13 +17,11 @@ import reactor.core.publisher.Mono;
 public class RelationshipsServiceImpl implements RelationshipsService {
     private final ReactiveRelationshipsRepository relationshipsRepository;
     private final ReactiveDataEntityRelationshipRepository dataEntityRelationshipRepository;
-    private final ReactiveRelationshipsDetailsRepository relationshipsDetailsRepository;
     private final RelationshipMapper relationshipMapper;
-    private final RelationshipDetailsMapper relationshipDetailsMapper;
 
     @Override
-    public Mono<DataEntityRelationshipList> getRelationsByDatasetId(final Long dataEntityId,
-                                                                 final RelationshipsType type) {
+    public Mono<DataEntityRelationshipDetailsList> getRelationsByDatasetId(final Long dataEntityId,
+                                                                           final RelationshipsType type) {
         return relationshipsRepository.getRelationsByDatasetIdAndType(dataEntityId, type)
             .collectList()
             .map(relationshipMapper::mapListToRelationshipList);
@@ -39,16 +35,16 @@ public class RelationshipsServiceImpl implements RelationshipsService {
     }
 
     @Override
-    public Mono<DataEntityERDRelationshipDetails> getERDRelationshipById(final Long relationshipId) {
-        return relationshipsDetailsRepository.getRelationshipByIdAndType(relationshipId, RelationshipsType.ERD)
+    public Mono<DataEntityRelationshipDetails> getERDRelationshipById(final Long relationshipId) {
+        return relationshipsRepository.getRelationshipByIdAndType(relationshipId, RelationshipsType.ERD)
             .switchIfEmpty(Mono.error(() -> new NotFoundException("Relationship", relationshipId)))
-            .map(relationshipDetailsMapper::mapToDataEntityERDRelationshipDetails);
+            .map(relationshipMapper::mapToDatasetRelationshipDetails);
     }
 
     @Override
-    public Mono<DataEntityGraphRelationshipDetails> getGraphRelationshipById(final Long relationshipId) {
-        return relationshipsDetailsRepository.getRelationshipByIdAndType(relationshipId, RelationshipsType.GRAPH)
+    public Mono<DataEntityRelationshipDetails> getGraphRelationshipById(final Long relationshipId) {
+        return relationshipsRepository.getRelationshipByIdAndType(relationshipId, RelationshipsType.GRAPH)
             .switchIfEmpty(Mono.error(() -> new NotFoundException("Relationship", relationshipId)))
-            .map(relationshipDetailsMapper::mapToDataEntityGraphRelationshipDetails);
+            .map(relationshipMapper::mapToDatasetRelationshipDetails);
     }
 }
