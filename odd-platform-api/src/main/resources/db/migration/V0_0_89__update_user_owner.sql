a292d5f96d0a5e915a79127e4019442a7c7b32c5
Vladysl <45620393+Vladysl@users.noreply.github.com>
2024-04-17 18:40:55 +0300

    timezone fix (#1662)

--- odd-platform-api/src/main/resources/db/migration/V0_0_89__update_user_owner.sql
+++ odd-platform-api/src/main/resources/db/migration/V0_0_89__update_user_owner.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS owner_association_request_activity
     owner_association_request_id    bigint,
     event_type                      varchar(64)                 NOT NULL,
     status                          varchar(64)                 NOT NULL,
-    created_at                      timestamp without time zone NOT NULL DEFAULT NOW(),
+    created_at                      timestamp without time zone NOT NULL DEFAULT (NOW() AT TIME ZONE ('UTC')),
     status_updated_by               varchar(512),
 
     CONSTRAINT owner_association_request_activity_fk_owner_association_request FOREIGN KEY (owner_association_request_id) REFERENCES owner_association_request (id)
